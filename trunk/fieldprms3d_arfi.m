4ba5a3b45f04720da586f5b9c324b31ad17a3f16
mlp6 <mlp6@f2fa97c2-6b19-0410-a95d-9979e3cdc715>
2006-10-18 16:18:36 +0000

    Added the PL3.5 Elegra probe that Kathy is using for the liver studies.
    git-svn-id: https://hogwarts.egr.duke.edu/svn/fem@4 f2fa97c2-6b19-0410-a95d-9979e3cdc715

--- trunk/fieldprms3d_arfi.m
+++ trunk/fieldprms3d_arfi.m
@@ -115,6 +115,29 @@ switch FIELD_PARAMS.Transducer
 		% Gianmarco's is very broadband, so I will artifcially put
 		% it at 2.0
 		fractionalBandwidth = 2.0;
+	case 'pl35elegra'
+		disp('Transducer: 3.5pl28 elegra');
+		no_elements_y=1;
+		width=.2e-3;
+		kerf=0.02e-3;
+		pitch = width + kerf;
+		height=14e-3;
+		% define # of elements based on F/#
+		no_elements=(FIELD_PARAMS.focus(3)/FIELD_PARAMS.Fnum)/pitch;
+    if no_elements > 128
+      no_elements=128
+      disp('too many elements')
+    end
+		% lens focus
+		Rfocus=60e-3;
+		% mathematically sub-dice elements to make them ~1 lambda dimensions
+		no_sub_y=height/width;
+		no_sub_x=1;
+		% define the transducer handle
+		Th = xdc_focused_multirow (no_elements,width,no_elements_y,height, ...  
+							kerf,kerf,Rfocus,no_sub_x,no_sub_y,FIELD_PARAMS.focus);           
+		% define the fractional bandwidth
+		fractionalBandwidth = 0.5;
 end;
 
 % frequency is now an input variable
