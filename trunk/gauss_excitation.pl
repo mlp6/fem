230f32486a7e1ab90604aeae6527092d2baf1232
mlp6 <mlp6@f2fa97c2-6b19-0410-a95d-9979e3cdc715>
2007-06-04 21:00:55 +0000

    (1) Renamed tags -> stable. (2) Fixed some comment bugs in gauss_excitation.pl
    git-svn-id: https://hogwarts.egr.duke.edu/svn/fem@29 f2fa97c2-6b19-0410-a95d-9979e3cdc715

--- trunk/gauss_excitation.pl
+++ trunk/gauss_excitation.pl
@@ -12,7 +12,7 @@
 
 # assuming that the center of the excitation is at (0,0)
 $sigma_x = 0.1; # s.d. (cm)
-$sigma_y = $sigma_x;  # s.d. (cm)
+$sigma_y = 0.2;  # s.d. (cm)
 $A = 1;  # Gaussian amplitude (this can be scaled by the load curve)
 $Amin = 0.05; # amplitude threshold to stop creating point loads 
 $zmax = -1.0; # "top" of the push (cm)
@@ -30,15 +30,15 @@ if (-e 'gauss_excitation.dyn') {
 }
 else {
     open(EXCFILE,'> gauss_excitation.dyn');	
-    print EXCFILE "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ \n";
-    print EXCFILE "$ gauss_excitation.pl parameters\n";
-    print EXCFILE "$ zmin = $zmin\n";
-    print EXCFILE "$ zmax = $zmax\n";
-    print EXCFILE "$ sigma_x = $sigma_x\n";
-    print EXCFILE "$ sigma_y = $sigma_y\n";
-    print EXCFILE "$ A = $A\n";
-    print EXCFILE "$ Amin = $Amin\n";
-    print EXCFILE "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ \n";
+    print EXCFILE "\$ -------------------------------\n";
+    print EXCFILE "\$ gauss_excitation.pl parameters\n";
+    print EXCFILE "\$ zmin = $zmin\n";
+    print EXCFILE "\$ zmax = $zmax\n";
+    print EXCFILE "\$ sigma_x = $sigma_x\n";
+    print EXCFILE "\$ sigma_y = $sigma_y\n";
+    print EXCFILE "\$ A = $A\n";
+    print EXCFILE "\$ Amin = $Amin\n";
+    print EXCFILE "\$ -------------------------------\n";
     print EXCFILE "*LOAD_NODE_POINT\n";
 }
 
