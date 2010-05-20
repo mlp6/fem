eb6d31bed3fabce85bef87ffa23026d9f47c2645
mlp6 <mlp6@f2fa97c2-6b19-0410-a95d-9979e3cdc715>
2010-05-20 21:07:19 +0000

    Fixed some problems with the Gaussian excitation python script:
    # GaussExc.py
    
    __author__ = "Mark Palmeri (mlp6)"
    __date__ = "2010-05-19"
    __version__ = "20100520"
    
    ###########################################################################################
    # 20100520
    #   * consolidated sigmas into one input tuple
    #   * corrected need for abs value on the symmetry searches
    #   * corrected the Guassian amplitude calculation to actually include the sigmas!
    #   * converted the 'fields' read from the nodefile to floats right off the bat
    ###########################################################################################
    
    
    
    git-svn-id: https://hogwarts.egr.duke.edu/svn/fem@72 f2fa97c2-6b19-0410-a95d-9979e3cdc715

--- dyna/GaussExc.py
+++ dyna/GaussExc.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
+# GaussExc.py
 
 __author__ = "Mark Palmeri (mlp6)"
 __date__ = "2010-05-19"
+__version__ = "20100520"
 
+###########################################################################################
+# 20100520
+#   * consolidated sigmas into one input tuple
+#   * corrected need for abs value on the symmetry searches
+#   * corrected the Guassian amplitude calculation to actually include the sigmas!
+#   * converted the 'fields' read from the nodefile to floats right off the bat
+###########################################################################################
+ 
 import os
 import sys
 import math
@@ -10,11 +20,10 @@ import math
 from optparse import OptionParser
 
 def main():
-    parser = OptionParser(usage="Generate *LOAD_NODE_POINT data with Gaussian weighting about dim1 = 0, dim2 = 0, extending through dim3.  All spatial units are in the unit system for the node definitions.\n\n%prog [OPTION]...")
+    parser = OptionParser(usage="Generate *LOAD_NODE_POINT data with Gaussian weighting about dim1 = 0, dim2 = 0, extending through dim3.  All spatial units are in the unit system for the node definitions.\n\n%prog [OPTIONS]...",version="%s" % __version__)
+#    parser = OptionParser(usage="Generate *LOAD_NODE_POINT data with Gaussian weighting about dim1 = 0, dim2 = 0, extending through dim3.  All spatial units are in the unit system for the node definitions.\n\n%prog [OPTIONS]...", version="%prog %s" % __version__)
     parser.add_option("--nodefile",dest="nodefile",help="Node definition file (*.dyn) [default = %default]",default="nodes.dyn")
-    parser.add_option("--sigma1",dest="sigma1",type="float",help="S.D. in dimension 1 [default = %default]",default=1.0)
-    parser.add_option("--sigma2",dest="sigma2",type="float",help="S.D. in dimension 2 [default = %default]",default=1.0)
-    parser.add_option("--sigma3",dest="sigma3",type="float",help="S.D. in dimension 3 [default = %default]",default=1.0)
+    parser.add_option("--sigma",dest="sigma",type="float",help="Standard devisions in 3 dims [default = %default]",nargs=3,default=(1.0,1.0,1.0))
     parser.add_option("--amp",dest="amp",type="float",help="Peak Gaussian amplitude [default = %default]",default=1.0)
     parser.add_option("--amp_cut",dest="amp_cut",type="float",help="Cutoff from peak amplitude to discard (so a lot of the nodes don't have neglible loads on them) [default = %default]",default=0.05)
     parser.add_option("--center",dest="center",type="float",help="Gaussian center [default = %default]",nargs=3,default=(0.0,0.0,-2.0))
@@ -25,9 +34,9 @@ def main():
 
     #pdb.set_trace()
     # setup the new output file with a very long, but unique, filename
-    loadfilename = "gauss_exc_sigma1_%.3f_sigma2_%.3f_sigma3_%.3f_center_%.3f_%.3f_%.3f_amp_%.3f_amp_cut_%.3f_%s.dyn" % (opts.sigma1,opts.sigma2,opts.sigma3,opts.center[0],opts.center[1],opts.center[2],opts.amp,opts.amp_cut,opts.sym)
+    loadfilename = "gauss_exc_sigma_%.3f_%.3f_%.3f_center_%.3f_%.3f_%.3f_amp_%.3f_amp_cut_%.3f_%s.dyn" % (opts.sigma[0],opts.sigma[1],opts.sigma[2],opts.center[0],opts.center[1],opts.center[2],opts.amp,opts.amp_cut,opts.sym)
     LOADFILE = open(loadfilename,'w')
-    LOADFILE.write("$ Generated using %s with the following options:\n" % sys.argv[0])
+    LOADFILE.write("$ Generated using %s (version %s) with the following options:\n" % (sys.argv[0],__version__))
     LOADFILE.write("$ %s\n" % opts)
 
     LOADFILE.write("*LOAD_NODE_POINT\n")
@@ -42,15 +51,16 @@ def main():
             i=i.rstrip('\n')
             # dyna scripts should be kicking out comma-delimited data; if not, then the user needs to deal with it
             fields=i.split(',')
+            fields = [float(i) for i in fields]
             # check for unexpected inputs and exit if needed (have user figure out what's wrong)
             if len(fields) != 4:
                 print("WARNING: An unexpected number of node definition columns encountered!")
                 print(fields)
                 sys.exit(1)
             # compute the Gaussian amplitude at the node
-            exp1 = math.pow(float(fields[1])-opts.center[0],2)
-            exp2 = math.pow(float(fields[2])-opts.center[1],2)
-            exp3 = math.pow(float(fields[3])-opts.center[2],2)
+            exp1 = math.pow((fields[1]-opts.center[0])/opts.sigma[0],2)
+            exp2 = math.pow((fields[2]-opts.center[1])/opts.sigma[1],2)
+            exp3 = math.pow((fields[3]-opts.center[2])/opts.sigma[2],2)
             nodeGaussAmp = opts.amp * math.exp(-(exp1 + exp2 + exp3))
 
             # write the point load only if the amplitude is above the cutoff
@@ -61,20 +71,20 @@ def main():
                 node_count += 1
                 # check for quarter symmetry force reduction (if needed)
                 if opts.sym is "qsym":
-                    if float(fields[1]) < opts.search_tol and float(fields[2]) < opts.search_tol:
+                    if math.fabs(fields[1]) < opts.search_tol and math.fabs(fields[2]) < opts.search_tol:
                         nodeGaussAmp = nodeGaussAmp/4
                         sym_node_count += 1
-                    elif float(fields[1]) < opts.search_tol or float(fields[2]) < opts.search_tol:
+                    elif math.fabs(fields[1]) < opts.search_tol or math.fabs(fields[2]) < opts.search_tol:
                         nodeGaussAmp = nodeGaussAmp/2
                         sym_node_count += 1
 
                 # check for half symmetry force reduction (if needed)
                 if opts.sym is "hsym":
-                    if float(fields[1]) < opts.search_tol:
+                    if math.fabs(fields[1]) < opts.search_tol:
                         nodeGaussAmp = nodeGaussAmp/2
                         sym_node_count += 1
                     
-                LOADFILE.write("%i,%.4f\n" % (int(fields[0]),nodeGaussAmp))
+                LOADFILE.write("%i,3,1,-%.4f\n" % (int(fields[0]),nodeGaussAmp))
                 
     # wrap everything up
     NODEFILE.close()
