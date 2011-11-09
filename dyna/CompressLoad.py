5774f366cf1c35e6879711d20b923ce39b9548cf
mlp6 <mlp6@f2fa97c2-6b19-0410-a95d-9979e3cdc715>
2011-11-09 19:44:58 +0000

    Modified the code to import SortNodeIDs and extractPlanes from bc.py instead of having duplicate sub-functions floating around that may conflict with one another.
    git-svn-id: https://hogwarts.egr.duke.edu/svn/fem@92 f2fa97c2-6b19-0410-a95d-9979e3cdc715

--- dyna/CompressLoad.py
+++ dyna/CompressLoad.py
@@ -3,17 +3,22 @@
 CompressLoad.py - Generate compression conditions for the top surface of the specified mesh.  Search through the provided node file, extract the top layer of nodes and write out point loads for each matching node. Nodes are written in spatially-sorted order
 
 This code was based on the older CompressLoad.pl and on bc.py
+
+MODIFICATION HISTORY:
+v0.2 (Mark Palmeri [2011-11-09])
+Imported SortNodeIDs and extractPlane from bc.py instead of having them copied here.
 '''
 
 __author__ = "Nick Bottenus (nbb5)"
-__date__ = "2011-10-06"
-__version__ = "0.1"
-
+#__date__ = "2011-10-06"
+__date__ = "2011-11-09"
+__version__ = "0.2"
 
 def main():
     import pdb 
     import os,sys,math
     import numpy as n
+    from bc import SortNodeIDs, extractPlanes
 
     if sys.version < '2.6':
         sys.exit("ERROR: Requires Python >= v2.6")
@@ -51,64 +56,6 @@ def main():
     # close all of our files open for read/write
     LOADFILE.close()
 
-#############################################################################################################################
-def SortNodeIDs(nic):
-    '''
-    Sort the node IDs by spatial coordinates into a 3D matrix and return the corresponding axes
-
-    INPUTS:
-        nic - nodeIDcoords (n matrix [# nodes x 4, dtype = i4,f4,f4,f4])
-
-    OUTPUTS:
-        SortedNodeIDs - n matrix (x,y,z)
-        x - array
-        y - array
-        z - array
-    '''
-
-    import numpy as n
-
-    axes = [n.unique(nic['x']), n.unique(nic['y']), n.unique(nic['z'])]
-    
-    # test to make sure that we have the right dimension (and that precision issues aren't adding extra unique values)
-    if len(nic) != (axes[0].size * axes[1].size * axes[2].size):
-        sys.exit('ERROR: Dimension mismatch - possible precision error when sorting nodes (?)') 
-
-    # sort the nodes by x, y, then z columns
-    I = nic.argsort(order=('x','y','z')) 
-    snic = nic[I]
-    snic = snic.reshape((axes[0].size,axes[1].size,axes[2].size))  
-    
-    return [snic, axes]
-
-#############################################################################################################################
-def extractPlane(snic,axes,plane):
-    '''
-    Extract the node IDs on a specified plane from a sorted node ID & coordinate 3D array.
-
-    INPUTS:
-        snic - sorted node IDs & coordinates array
-        axes - list of unique coordinates in the x, y, and z dimensions
-        plane - list:
-            index - index of the plane to extract (x=0, y=1, z=2)
-            coord - coordinate of the plane to extract (must exist in axes list)
-
-    OUPUTS:
-        planeNodeIDs - spatially-sorted 2D node IDs on the specified plane
-        
-    EXAMPLE: planeNodeIDs = extractPlane(snic,axes,(0,-0.1))
-    '''
-    if plane[0] == 0:
-        planeNodeIDs = snic[axes[plane[0]] == plane[1],:,:]
-    elif plane[0] == 1: 
-        planeNodeIDs = snic[:,axes[plane[0]] == plane[1],:]
-    elif plane[0] == 2: 
-        planeNodeIDs = snic[:,:,axes[plane[0]] == plane[1]]
-    else:
-        sys.exit("ERROR: Specified plane index to extract does not exist")
-
-    planeNodeIDs = planeNodeIDs.squeeze()
-    return planeNodeIDs
 #############################################################################################################################
 def writeNodeLoads(LOADFILE,planeNodeIDs,dofs):
     import pdb
