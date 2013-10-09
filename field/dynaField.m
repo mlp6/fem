727671ad9dd68c9ec065681ce730b6bf71f0fa40
Ningrui Li <ningrui.li@duke.edu>
2013-10-09 12:49:19 -0400

    instead of using a parfor loop, used spmd to and for loops to have each worker run dynaField.m for different parts of the measurementPoints array. resulting runtime decreased by approximately half

--- field/dynaField.m
+++ field/dynaField.m
@@ -1,7 +1,7 @@
 function [intensity,FIELD_PARAMS]=dynaField(FIELD_PARAMS)
 % function [intensity,FIELD_PARAMS]=dynaField(FIELD_PARAMS)
 % --------------------------------------------------------------------------
-% Generate intensity values at the nodal locations for 
+% Generate intensity values at the nodal locations for
 % conversion to force and input into the dyna deck (performed by make_asc.m)
 % INPUTS:
 % FIELD_PARAMS.alpha (float) - absoprtion (dB/cm/MHz)
@@ -9,8 +9,8 @@ function [intensity,FIELD_PARAMS]=dynaField(FIELD_PARAMS)
 % FIELD_PARAMS.Fnum (float) - F/#
 % FIELD_PARAMS.focus - [x y z] (m)
 % FIELD_PARAMS.Frequency (float) - push frequency (MHz)
-%								 6.67 (VF10-5)
-%								 4.21 (VF7-3)
+% 6.67 (VF10-5)
+% 4.21 (VF7-3)
 % FIELD_PARAMS.Transducer (string) - 'vf105'; select the
 % transducer-dependent parameters to use for the simulation
 % FIELD_PARAMS.Impulse (string) - 'guassian','exp'; use a Guassian function
@@ -29,7 +29,7 @@ function [intensity,FIELD_PARAMS]=dynaField(FIELD_PARAMS)
 % Mark 01/31/05
 % --------------------------------------------------------------------------
 % Cleaned up the order of the definiting of parameters in the
-% code to make it more readable.  The code now allows for the
+% code to make it more readable. The code now allows for the
 % transducer definition and the impulse response to be passed
 % as input variables.
 % Mark 06/16/05
@@ -55,22 +55,31 @@ function [intensity,FIELD_PARAMS]=dynaField(FIELD_PARAMS)
 % Mark 2013-02-09
 % --------------------------------------------------------------------------
 
+% numWorkers = matlabpool('size');
+% isPoolOpen = (numWorkers > 0);
+% if(~isPoolOpen)
+%     matlabpool open;
+% end
+
 field_init(-1)
+%field_debug(1);
+
 disp('Starting the Field II simulation');
 %set_field('use_triangles',1);
 %set_field('use_lines',1);
 
 % define transducer-independent parameters
-set_field('c',FIELD_PARAMS.soundSpeed); 
+set_field('c',FIELD_PARAMS.soundSpeed);
 set_field('fs',FIELD_PARAMS.samplingFrequency);
 
 % define transducer-dependent parameters
 eval(sprintf('[Th,impulseResponse]=%s(FIELD_PARAMS);',FIELD_PARAMS.Transducer));
+
 % define the impulse response
 xdc_impulse(Th,impulseResponse);
 
 % define the excitation pulse
-exciteFreq=FIELD_PARAMS.Frequency*1e6;  % Hz
+exciteFreq=FIELD_PARAMS.Frequency*1e6; % Hz
 ncyc=50;
 texcite=0:1/FIELD_PARAMS.samplingFrequency:ncyc/exciteFreq;
 excitationPulse=sin(2*pi*exciteFreq*texcite);
@@ -78,7 +87,7 @@ xdc_excitation(Th,excitationPulse);
 
 % set attenuation
 Freq_att=FIELD_PARAMS.alpha*100/1e6; % FIELD_PARAMS in dB/cm/MHz
-att_f0=exciteFreq; 
+att_f0=exciteFreq;
 att=Freq_att*att_f0; % set non freq. dep. to be centered here
 set_field('att',att);
 set_field('Freq_att',Freq_att);
@@ -90,41 +99,37 @@ set_field('use_att',1);
 StartTime = fix(clock);
 disp(sprintf('Start Time: %i:%i',StartTime(4),StartTime(5)));
 tic;
-% EstCount = 1000;  % number of calculations to average over to make calc time estimates
-% numNodes = size(FIELD_PARAMS.measurementPoints,1);
+
+% EstCount = 1000; % number of calculations to average over to make calc time estimates
+
+numNodes = size(FIELD_PARAMS.measurementPoints,1);
 % progressPoints = 0:10000:numNodes;
-% 
+%
 % for i=1:1000,
-%     field_init(-1)
-%     if ~isempty(intersect(i,progressPoints)),
-%         disp(sprintf('Processed %.1f%%',i*100/numNodes));
-%     end;
-%     if i==1
-%        tic;
-%     end;
-%     [pressure, startTime] = calc_hp(Th, FIELD_PARAMS.measurementPoints(i,:));
-%     intensity(i)=sum(pressure.*pressure);
+%   if ~isempty(intersect(i,progressPoints)),
+%       disp(sprintf('Processed %.1f%%',i*100/numNodes));
+%   end;
+%   if i==1
+%       tic;
+%   end;
+%   [pressure, startTime] = calc_hp(Th, FIELD_PARAMS.measurementPoints(i,:));
+%   intensity(i)=sum(pressure.*pressure);
 % end
 
-tic;
+%tic;
 test = Th; %not sure why, but putting Th directly inside parfor loop gives an error that Th is undefined, however, setting a different variable equal to Th and using that works.
 
-numWorkers = matlabpool('size');
-isPoolOpen = (numWorkers > 0);
-if(~isPoolOpen)
-    matlabpool open;
-end
-
-parfor i=1:2
-    field_init(-1)
+for i=1:numNodes
+%parfor i=1:100
     [pressure, startTime] = calc_hp(test, FIELD_PARAMS.measurementPoints(i,:));
-    %intensity(i)=sum(pressure.*pressure);
+    intensity(i)=sum(pressure.*pressure);
 end
+%intensity
 
-matlabpool close;
+% matlabpool close;
 
-CalcTime = toc; % s
-ActualRunTime = CalcTime/60; % min
-disp(sprintf('Actual Run Time = %.3f m\n',ActualRunTime));
+% CalcTime = toc; % s
+% ActualRunTime = CalcTime/60; % min
+% disp(sprintf('Actual Run Time = %.3f m\n',ActualRunTime));
 
 field_end;
--- field/field2dyna.m
+++ field/field2dyna.m
@@ -3,8 +3,8 @@ function []=field2dyna(NodeName,alpha,Fnum,focus,Frequency,Transducer,Impulse)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % INPUT:
 % NodeName (string) - file name to read nodes from (e.g., nodes.dyn); needs to
-%                     be a comma-delimited file with header/footer lines that
-%                     start with *
+% be a comma-delimited file with header/footer lines that
+% start with *
 % alpha - 0.5, 1.0, etc. (dB/cm/MHz)
 % Fnum - F/# (e.g. 1.3)
 % focus - [x y z] (m) "Field" coordinates
@@ -19,7 +19,7 @@ function []=field2dyna(NodeName,alpha,Fnum,focus,Frequency,Transducer,Impulse)
 % field2dyna('nodes.dyn',0.5,1.3,[0 0 0.02],7.2,'vf105','gaussian');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-addpath('/home/mlp6/matlab/Field_II_7.10');
+addpath('/home/nl91/lab/Field_II_7.10');
 
 % read in the nodes
 fid = fopen(NodeName,'r');
@@ -30,7 +30,7 @@ measurementPointsandNodes = cell2mat(measurementPointsandNodes);
 % skip node number, use just coords
 measurementPoints=measurementPointsandNodes(:,2:4);
 
-% check to see if there are nodes in the x = y = 0 plane to make sure that 
+% check to see if there are nodes in the x = y = 0 plane to make sure that
 % "on axis" intensities are properly captured
 check_on_axis(measurementPoints);
 
@@ -41,8 +41,8 @@ measurementPoints(:,3)=-measurementPoints(:,3);
 tmp=measurementPoints(:,1:2);
 measurementPoints(:,1:2)=[tmp(:,2) tmp(:,1)];
 
-% convert from cm -> m 
-measurementPoints=measurementPoints/100; 
+% convert from cm -> m
+measurementPoints=measurementPoints/100;
 
 % create a variable structure to pass to dynaField
 FIELD_PARAMS.measurementPointsandNodes = measurementPointsandNodes;
@@ -58,10 +58,26 @@ FIELD_PARAMS.Impulse = Impulse;
 FIELD_PARAMS.soundSpeed=1540;
 FIELD_PARAMS.samplingFrequency = 200e6;
 
-% perform the field calculation
-[intensity,FIELD_PARAMS]=dynaField(FIELD_PARAMS);
+% small sample of measurementPoints testing
+FIELD_PARAMS.measurementPoints = measurementPoints(1:60000, :);
 
-% save intenstiy file
+% perform the field calculation
+
+tic;
+[intensity,FIELD_PARAMS]=dynaField(FIELD_PARAMS);
+CalcTime = toc; % s
+ActualRunTime = CalcTime/60; % min
+disp(sprintf('Actual Run Time = %.3f m\n',ActualRunTime));
+
+tic;
+[intensityParallel,FIELD_PARAMS]=test(FIELD_PARAMS);
+CalcTime = toc; % s
+ActualRunTime = CalcTime/60; % min
+disp(sprintf('Parallel Actual Run Time = %.3f m\n',ActualRunTime));
+
+min(intensity == intensityParallel)
+
+% save intensity file
 eval(sprintf('save dyna-I-f%.2f-F%.1f-FD%.3f-a%.1f.mat intensity FIELD_PARAMS',Frequency,Fnum,focus(3),alpha));
 
 disp('The next step is to run makeLoadsTemps.');
@@ -79,7 +95,6 @@ ylocs = unique(measurementPoints(2,:));
 
 % test for x and y locations that are at 0 (imaging plane), and if both don't exist, then display a warning
 if((max(xlocs==0) + max(ylocs==0)) < 2),
-    warning('There are not nodes in the lateral / elevation plane = 0 (imaging plane).  This can lead to inaccurate representations of the intensity fields!!');
+    warning('There are not nodes in the lateral / elevation plane = 0 (imaging plane). This can lead to inaccurate representations of the intensity fields!!');
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
