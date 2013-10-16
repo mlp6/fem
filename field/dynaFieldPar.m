3fbe1517d446168ce261cc8a41161781c763c0ca
Ningrui Li <ningrui.li@duke.edu>
2013-10-16 10:21:30 -0400

    Changed save to dsave for parallel variables. Also, changed name of parallel function from test.m to dynaFieldPar.m

---
+++ field/dynaFieldPar.m
@@ -0,0 +1,28 @@
+function [intensity,FIELD_PARAMS] = dynaFieldPar(FIELD_PARAMS)
+% tic;
+numWorkers = matlabpool('size');
+isPoolOpen = (numWorkers > 0);
+if(~isPoolOpen)
+    matlabpool open;
+end
+
+spmd
+    codistPoints = codistributed(FIELD_PARAMS.measurementPoints, codistributor('1d', 1));
+    a = size(FIELD_PARAMS.measurementPoints);
+    switch labindex
+        case 1
+            FIELD_PARAMS.measurementPoints = getLocalPart(codistPoints);
+            [intensityCodist,FIELD_PARAMS]=dynaField(FIELD_PARAMS);
+        case 2
+            FIELD_PARAMS.measurementPoints = getLocalPart(codistPoints);
+            [intensityCodist,FIELD_PARAMS]=dynaField(FIELD_PARAMS);
+    end 
+    intensityDist = codistributed.build(intensityCodist, codistributor1d(2, codistributor1d.unsetPartition, [1 a(1)]));
+end
+intensityGather = gather(intensityDist);
+intensity = intensityGather(:);
+matlabpool close
+
+% CalcTime = toc; % s
+% ActualRunTime = CalcTime/60; % min
+% disp(sprintf('Parallel Actual Run Time = %.3f m\n',ActualRunTime));
