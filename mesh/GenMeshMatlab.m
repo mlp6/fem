4c6daa9463452338655d2cb702eecb6e0b3be636
Ningrui Li (KNightingale 1/2013) <nl91@ali.egr.duke.edu>
2013-12-04 12:58:41 -0500

    initial start on mesh generation

---
+++ mesh/GenMeshMatlab.m
@@ -0,0 +1,40 @@
+function [x, y, z] = GenMeshMatlab(x1, y1, z1, x2, y2, z2, xEle, yEle, zEle)
+%GENMESHMATLAB 
+%   Using (x1, y1, z1) and (x2, y2, z2) as opposite corners of a meshgrid,
+%   creates nodes.dyn and elems.dyn using xEle, yEle, and zEle as the
+%   number of x, y, and z elements respectively.
+%   Currently assuming that x1 > x2, y1 > y2, z1 > z2.
+    
+
+    % based on the node ID of the first corner, nid1, the other node ids
+    % can be given by:
+    % nid1
+    % nid1 + 1 (going down)
+    % nid1 + 1 + (xEle + 1) (going down then right)
+    % nid1 + (xEle + 1) (going right)
+    % This must be done from 1 to 103 (which is zEle + 1)
+    % above gives vertices defining an element face in the order given by
+    % the right hand rule (outward normal).
+    % opposite face:
+    % (xEle + 1)(yEle + 1) + nid1 
+    % (xEle + 1)(yEle + 1) + nid1 + 1 (going down)
+    % (xEle + 1)(yEle + 1) + nid1 + 1 + (xEle + 1) (going down then right)
+    % (xEle + 1)(yEle + 1) + nid1 + (xEle + 1) (going right)
+
+    
+    [y, z, x] = meshgrid(linspace(x1, x2, xEle + 1), ...
+                         linspace(y1, y2, yEle + 1), ...
+                         linspace(z1, z2, zEle + 1));
+    counter = 1;
+    for zIter = 1:(zEle+1)
+        for yIter = 1:(yEle+1)
+            for xIter = 1:(xEle+1)
+                fprintf('%6.4f %6.4f %6.4f\n', x(xIter, yIter, zIter), y(xIter, yIter, zIter), z(xIter, yIter, zIter))
+                counter = counter + 1;
+                if (counter == 100)
+                    return;
+            end
+        end
+    end
+end
+
