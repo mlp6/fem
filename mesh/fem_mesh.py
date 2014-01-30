82a3d6be67e0a5ad571aa2ee279c7aa1d339618a
Mark Palmeri <mlp6@duke.edu>
2014-01-29 22:21:46 -0500

    new general module for meshing functions

---
+++ mesh/fem_mesh.py
@@ -0,0 +1,23 @@
+"""
+fem_mesh.py
+
+Generic functions for all meshing functions
+"""
+
+__author__ = "Mark Palmeri"
+__email__ = "mlp6@duke.edu"
+__version__ = "20140129"
+
+
+def check_version():
+    """
+    Check that at least python2.7 is being used, and recommend upgrading to
+    python3 if 2.bit_length(x is being used
+    """
+    import sys
+
+    if sys.version_info[:2] < (2, 7):
+        sys.exit("ERROR: Requires Python >= 2.7")
+
+    if sys.version_info[0] < 3:
+        print("WARNING: It is recommended that you upgrade to python3!")
