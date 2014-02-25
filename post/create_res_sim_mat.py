a079f5537aacd315c5f5f0388ffddc70597b8f65
Mark Palmeri <mlp6@duke.edu>
2014-02-25 13:57:22 -0500

    reformatted code for expansion

--- post/create_res_sim_mat.py
+++ post/create_res_sim_mat.py
@@ -1,47 +1,89 @@
-#!/usr/local/bin/python2.7
 '''
-create_res_sim_mat.py - create res_sim.mat file from disp.dat
+create_res_sim_mat.py
 
-This was originally called from StructPost, but now is a stand-along Python script
+Create res_sim.mat file from disp.dat / vel.dat
+(This was originally called from StructPost, but now is a stand-along Python
+script.)
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Mark L. Palmeri
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 '''
 
 __author__ = "Mark Palmeri"
 __version__ = "0.2a"
-__email__ = "mark.palmeri@duke.edu"
+__email__ = "mlp6@duke.edu"
 __license__ = "MIT"
 
+
 def main():
-    import os,sys
+    import os
+    import sys
 
     if sys.version < '2.7':
         sys.exit("ERROR: Requires Python >= v2.7")
 
-    import argparse
+    args = read_cli()
 
-    # lets read in some command-line arguments
-    parser = argparse.ArgumentParser(description="Generate res_sim.mat from disp.dat",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--dispout",help="name of the binary displacement output file",default="disp.dat")
-    parser.add_argument("--ressim",help="name of the matlab output file",default="res_sim.mat")
-    parser.add_argument("--fempath",help="path to the FEM post-processing scripts",default="/radforce/mlp6/fem/post")
-    parser.add_argument("--nodedyn",help="ls-dyna node definition file",default="nodes.dyn")
-    parser.add_argument("--dynadeck",help="ls-dyna input deck",default="dynadeck.dyn")
-
-    args = parser.parse_args()
-    dispout = args.dispout
-    ressim = args.ressim
-    fempath = args.fempath
-    nodedyn = args.nodedyn
-    dynadeck = args.dynadeck
-
-    matfile = open('runmatlab.m','w')
-    matfile.write('addpath(\'%s\');\n' % fempath)
-    matfile.write('createsimres(\'%s\',\'%s\',\'%s\');\n' % (dispout,nodedyn,dynadeck))
+    matfile = open('runmatlab.m', 'w')
+    matfile.write('addpath(\'%s\');\n' % args.fempath)
+    matfile.write('createsimres(\'%s\',\'%s\',\'%s\');\n' % (args.dispout,
+                                                             args.nodedyn,
+                                                             args.dynadeck))
     matfile.close()
 
     os.system('matlab -nodesktop -nosplash < runmatlab.m')
 
-    if not os.path.exists(ressim):
-        sys.exit('ERROR: %s not successfully created' % ressim)
+    if not os.path.exists(args.ressim):
+        sys.exit('ERROR: %s not successfully created' % args.ressim)
 
 if __name__ == "__main__":
     main()
+
+
+def read_cli():
+    """
+    read in command line arguments
+    """
+
+    import argparse as ap
+
+    par = ap.ArgumentParser(description="Generate res_sim.mat from disp.dat",
+                            formatter_class=ap.ArgumentDefaultsHelpFormatter)
+    par.add_argument("--dispout",
+                     help="name of the binary displacement output file",
+                     default="disp.dat")
+    par.add_argument("--ressim",
+                     help="name of the matlab output file",
+                     default="res_sim.mat")
+    par.add_argument("--fempath",
+                     help="path to the FEM post-processing scripts",
+                     default="/radforce/mlp6/fem/post")
+    par.add_argument("--nodedyn",
+                     help="ls-dyna node definition file",
+                     default="nodes.dyn")
+    par.add_argument("--dynadeck",
+                     help="ls-dyna input deck",
+                     default="dynadeck.dyn")
+
+    args = par.parse_args()
+
+    return args
