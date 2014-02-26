38ae206500295099635a95a4a609e38b557b9ffd
Mark Palmeri <mlp6@duke.edu>
2014-02-26 11:33:56 -0500

    trying to fix x=y=0 check

--- field/field2dyna.m
+++ field/field2dyna.m
@@ -111,8 +111,9 @@ function check_on_axis(measurementPoints)
 xlocs = unique(measurementPoints(1,:));
 ylocs = unique(measurementPoints(2,:));
 
-% test for x and y locations that are at 0 (imaging plane), and if both don't exist, then display a warning
-if((max(xlocs==0) + max(ylocs==0)) < 2),
+% test for x and y locations that are at 0 (imaging plane), and if both don't
+% exist, then display a warning
+if((max(xlocs==0.0) + max(ylocs==0.0)) < 2),
     warning('There are not nodes in the lateral / elevation plane = 0 (imaging plane). This can lead to inaccurate representations of the intensity fields!!');
 end
 end
