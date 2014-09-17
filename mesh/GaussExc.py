6320d5b6192119d7f1ad62c8cb9fff5936e4ed45
Mark Palmeri <mlp6@duke.edu>
2014-09-17 18:11:48 -0400

    syntax cleaning

--- mesh/GaussExc.py
+++ mesh/GaussExc.py
@@ -41,16 +41,19 @@ def main():
 
     opts = read_cli()
 
-    #pdb.set_trace()
     # setup the new output file with a very long, but unique, filename
-    loadfilename = "gauss_exc_sigma_%.3f_%.3f_%.3f_center_%.3f_%.3f_%.3f_amp_%.3f_amp_cut_%.3f_%s.dyn" % (opts.sigma[0],opts.sigma[1],opts.sigma[2],opts.center[0],opts.center[1],opts.center[2],opts.amp,opts.amp_cut,opts.sym)
+    loadfilename = ("gauss_exc_sigma_%.3f_%.3f_%.3f_center_%.3f_%.3f_%.3f_amp_%.3f_amp_cut_%.3f_%s.dyn" %
+                    (opts.sigma[0], opts.sigma[1], opts.sigma[2],
+                     opts.center[0], opts.center[1], opts.center[2],
+                     opts.amp, opts.amp_cut, opts.sym))
     LOADFILE = open(loadfilename, 'w')
     LOADFILE.write("$ Generated using %s:\n" % sys.argv[0])
     LOADFILE.write("$ %s\n" % opts)
 
     LOADFILE.write("*LOAD_NODE_POINT\n")
 
-    # loop through all of the nodes and see which ones fall w/i the Gaussian excitation field
+    # loop through all of the nodes and see which ones fall w/i the Gaussian
+    # excitation field
     sym_node_count = 0
     node_count = 0
     NODEFILE = open(opts.nodefile,'r')
@@ -58,32 +61,35 @@ def main():
         # make sure not to process comment and command syntax lines
         if i[0] != "$" and i[0] != "*":
             i=i.rstrip('\n')
-            # dyna scripts should be kicking out comma-delimited data; if not, then the user needs to deal with it
+            # dyna scripts should be kicking out comma-delimited data; if not,
+            # then the user needs to deal with it
             fields=i.split(',')
             fields = [float(i) for i in fields]
-            # check for unexpected inputs and exit if needed (have user figure out what's wrong)
+            # check for unexpected inputs and exit if needed (have user figure
+            # out what's wrong)
             if len(fields) != 4:
-                print("WARNING: An unexpected number of node definition columns encountered!")
+                print("ERROR: Unexpected number of node columns")
                 print(fields)
                 sys.exit(1)
             # compute the Gaussian amplitude at the node
-            exp1 = math.pow((fields[1]-opts.center[0])/opts.sigma[0],2)
-            exp2 = math.pow((fields[2]-opts.center[1])/opts.sigma[1],2)
-            exp3 = math.pow((fields[3]-opts.center[2])/opts.sigma[2],2)
+            exp1 = math.pow((fields[1]-opts.center[0])/opts.sigma[0], 2)
+            exp2 = math.pow((fields[2]-opts.center[1])/opts.sigma[1], 2)
+            exp3 = math.pow((fields[3]-opts.center[2])/opts.sigma[2], 2)
             nodeGaussAmp = opts.amp * math.exp(-(exp1 + exp2 + exp3))
 
             # write the point load only if the amplitude is above the cutoff
             # dyna input needs to be limited in precision
-            #pdb.set_trace()
             if nodeGaussAmp > opts.amp*opts.amp_cut:
 
                 node_count += 1
                 # check for quarter symmetry force reduction (if needed)
                 if opts.sym == 'qsym':
-                    if math.fabs(fields[1]) < opts.search_tol and math.fabs(fields[2]) < opts.search_tol:
+                    if (math.fabs(fields[1]) < opts.search_tol and
+                       math.fabs(fields[2]) < opts.search_tol):
                         nodeGaussAmp = nodeGaussAmp/4
                         sym_node_count += 1
-                    elif math.fabs(fields[1]) < opts.search_tol or math.fabs(fields[2]) < opts.search_tol:
+                    elif (math.fabs(fields[1]) < opts.search_tol or
+                          math.fabs(fields[2]) < opts.search_tol):
                         nodeGaussAmp = nodeGaussAmp/2
                         sym_node_count += 1
                 # check for half symmetry force reduction (if needed)
@@ -94,7 +100,8 @@ def main():
                 elif opts.sym != 'none':
                     sys.exit('ERROR: Invalid symmetry option specified.')
 
-                LOADFILE.write("%i,3,1,-%.4f\n" % (int(fields[0]),nodeGaussAmp))
+                LOADFILE.write("%i,3,1,-%.4f\n" % (int(fields[0]),
+                                                   nodeGaussAmp))
 
     # wrap everything up
     NODEFILE.close()
