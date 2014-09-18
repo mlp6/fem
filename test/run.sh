7996cfb78acc175af026eda3be7fef7d5c804c32
Mark Palmeri <mlp6@duke.edu>
2014-09-18 00:21:20 -0400

    quit Matlab when done

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,4 @@
 python ../mesh/GenMesh.py --xyz -0.5 0.0 0.0 1.0 -3.0 0.0 --numElem 10 20 60
 python ../mesh/bc.py
 python ../mesh/GaussExc.py --sigma 0.25 0.25 0.75 --center 0.0 0.0 -1.5 
-matlab -nodesktop -nosplash -r "addpath('/home/mlp6/git/fem/field'); field2dyna('nodes.dyn',0.5,1.0,[0.0 0.0 0.02],7.2,'vf105','gaussian')"
+matlab -nodesktop -nosplash -r "addpath('/home/mlp6/git/fem/field'); field2dyna('nodes.dyn',0.5,1.0,[0.0 0.0 0.02],7.2,'vf105','gaussian'); quit;"
