63c4260147013274c6c2e7d16e1e7e286c934a2c
Mark Palmeri <mlp6@duke.edu>
2014-09-23 14:22:51 -0400

    added ls-dyna-d run command

--- test/vf105/run.sh
+++ test/vf105/run.sh
@@ -1,5 +1,6 @@
 python ../../mesh/GenMesh.py --xyz -0.5 0.0 0.0 1.0 -3.0 0.0 --numElem 50 100 300
 python ../../mesh/bc.py
 matlab -nodesktop -nosplash -r "field2dyna('nodes.dyn',0.5,1.0,[0.0 0.0 0.02],7.2,'vf105','gaussian')"
+ls-dyna-d ncpu=2 i=vf105.dyn
 python ../../post/create_disp_dat.py
 python ../../post/create_res_sim_mat.py --dynadeck vf105.dyn
