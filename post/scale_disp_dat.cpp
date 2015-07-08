a665b19feaf5d0bb172a9cc39413feb927ecafbe
Mark Palmeri <mlp6@duke.edu>
2015-07-08 18:30:54 -0400

    add CLI input argument support

--- post/scale_disp_dat.cpp
+++ post/scale_disp_dat.cpp
@@ -2,25 +2,61 @@
 
 #include <fstream>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 using namespace std;
 
-int main () {
-    float scaleFactor = 0.1;
+int main (int argc, char **argv) {
+    float scaleFactor;
     int wordSize = 4;  // 32-bit float
     int headerCount = 3;  // number of header words
     int count = 0;  // float word count
     float value;
     int NUM_NODES;
 
-    // open files for reading and writing
-    fstream dispin ("disp.dat", ios::in | ios::binary);
-    fstream dispout ("disp_scaled.dat", ios::out | ios::binary);
+    extern char *optarg;
+    int opt = 0;
+    static char usage[] = "scale_disp_dat [-i disp.dat] [-o disp_scaled.dat] -s scaleFactor\n";
+    const char *dispinFilename = "disp.dat";
+    const char *dispoutFilename = "disp_scaled.dat";
 
+    if (argc == 1) {
+        printf("%s\n", usage);
+        exit(1);
+    }
+
+    while ((opt = getopt(argc, argv, "i:o:s:")) != -1) {
+        switch (opt) {
+            case 'i':
+                dispinFilename = optarg;
+                break;
+            case 'o':
+                dispoutFilename = optarg;
+                break;
+            case 's':
+                scaleFactor = strtof(optarg, NULL);
+                break;
+            default:
+                printf("%s\n", usage);
+                exit(1);
+        }
+    }
+
+    // let the user know what they told us to use
+    printf("Input File: %s\n", dispinFilename);
+    printf("Output File: %s\n", dispoutFilename);
+    printf("Scale Factor: %f\n", scaleFactor);
+
+    // open files for reading and writing
+    fstream dispin (dispinFilename, ios::in | ios::binary);
+    fstream dispout (dispoutFilename, ios::out | ios::binary);
+
+    // loop through the file, scaling only the displacement values
     while (dispin.read((char*)&value, wordSize)) {
         if (count == 0) {
             NUM_NODES = (int) value;
             dispout.write((char*)&value, wordSize);
-            // cout << "Number of nodes: " << NUM_NODES << '\n';
             count++;
             }
         else if (count < (headerCount + NUM_NODES)) {
@@ -34,6 +70,7 @@ int main () {
         }
     }
 
+    // close the files
     dispin.close();
     dispout.close();
 
