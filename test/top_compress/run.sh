46ef48633ba432e1f6d56b18d8b781d7068f8410
Mark Palmeri <mlp6@duke.edu>
2015-08-03 13:38:23 -0400

    update bc.py syntax to work with new PML-enabled bc.py

--- test/top_compress/run.sh
+++ test/top_compress/run.sh
@@ -1,5 +1,5 @@
 python ../../mesh/GenMesh.py --xyz -0.5 0.0 0.0 1.0 -3.0 0.0 --numElem 10 20 60
-python ../../mesh/bc.py --notop --sym none
+python ../../mesh/bc.py --nonreflect --notop --sym none
 python ../../mesh/TopLoad.py --amplitude -0.015
 ls-dyna-d ncpu=2 i=topcompress.dyn
 python ../../post/create_disp_dat.py
