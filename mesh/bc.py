8b152f970e145ff89b84c2fb6a86902e680f9dcd
Mark Palmeri <mlp6@duke.edu>
2015-10-29 11:14:30 -0400

    update rst metadata and LICENSE

--- mesh/bc.py
+++ mesh/bc.py
@@ -1,35 +1,17 @@
-#!/bin/env python
 """
-bc.py
+:mod:`bc` -- boundary conditions
+================================
 
-Apply boundary conditions to rectangular solid meshes (the majority of the FE
-sims); can handle quarter-, half-, and no symmetry models.
+.. module:: bc
+   :synopsis: apply boundary conditions to rectangular solid meshes
+   :license: Apache v2.0, see LICENSE for details
+   :copyright: Copyright 2015 Mark Palmeri
 
-There are 6 faces in these models; we need to:
- 1. Find all of them, and
- 2. Apply the appropriate BCs
+.. moduleauthor:: Mark Palmeri <mlp6@duke.edu>
 
-We'll loop through all of the nodes, see if they are on a face or edge, and
-then apply the appropriate BC.
+:Example: TEST
 
-EXAMPLE:
-
-=======
-Copyright 2015 Mark L. Palmeri (mlp6@duke.edu)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 """
-
 
 def main():
     import fem_mesh
