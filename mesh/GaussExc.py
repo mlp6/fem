6bfc1160a268bb8664c8299a45db8033b8576f75
Mark Palmeri <mlp6@duke.edu>
2016-02-01 14:33:46 -0500

    update header to rst format

--- mesh/GaussExc.py
+++ mesh/GaussExc.py
@@ -1,31 +1,15 @@
-""" Generate Gaussian ARF distribution
-
-Copyright 2015 Mark L. Palmeri (mlp6@duke.edu)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-20100520
-  * consolidated sigmas into one input tuple
-  * corrected need for abs value on the symmetry searches
-  * corrected the Gaussian amplitude calculation to actually include the sigmas!
-  * converted the 'fields' read from the nodefile to floats right off the bat
-
-2012-08-27 (Palmeri)
-  * Added 'none' symmetry option in case no symmetry is being used in the model
-
-Mark Palmeri
-mlp6@duke.edu
 """
+:mod:`GaussExc` -- Gaussian excitation
+======================================
+
+.. module:: GauseExc
+   :synopsis: generate Gaussian distribution of point loads
+   :license: Apache v2.0, see LICENSE for details
+   :copyright: Copyright 2016 Mark Palmeri
+
+.. moduleauthor:: Mark Palmeri <mlp6@duke.edu>
+"""
+
 from __future__ import absolute_import
 from __future__ import print_function
 
