7ddfd58ae2f803d84b10ef79aa9fa56109bea487
Mark Palmeri <mlp6@duke.edu>
2016-02-27 13:29:58 -0500

    rm non-pml gauss example

--- test/gauss/run.sh
+++
@@ -1,7 +0,0 @@
-python ../../mesh/GenMesh.py --xyz -1.5 0.0 0.0 1.5 -3.0 0.0 --numElem 75 75 150
-python ../../mesh/bc.py --nonreflect
-python ../../mesh/GaussExc.py --sigma 0.25 0.25 0.75 --center 0.0 0.0 -1.5 
-ls-dyna-s ncpu=2 i=gauss.dyn
-rm d3* 
-python ../../post/create_disp_dat.py --dat
-python ../../post/create_res_sim_mat.py --dynadeck gauss.dyn
