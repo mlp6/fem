16ab4bd61cb4d967b665f36cb397c91d612b33cd
Mark Palmeri <mlp6@duke.edu>
2016-03-03 16:29:58 -0500

    make run.py more readable

--- examples/gauss_qsym_pml/run.py
+++ examples/gauss_qsym_pml/run.py
@@ -8,7 +8,7 @@
 from time import ctime
 from os import environ, system
 from socket import gethostname
-import gen_mesh_bc
+from gen_mesh_bc import main as gen_mesh_bc
 from fem.mesh import GaussExc
 from fem.post.create_disp_dat import create_dat as create_disp_dat
 from fem.post.create_res_sim_mat import run as create_res_sim_mat
@@ -19,7 +19,7 @@ print('HOST: %s' % gethostname())
 DYNADECK='gauss_qsym_pml.dyn'
 NTASKS = environ.get('SLURM_NTASKS', '8')
 
-gen_mesh_bc.main()
+gen_mesh_bc()
 loads = GaussExc.read_process_nodes([0.25,0.25,0.75], [0.0,0.0,-1.5],)
 GaussExc.write_load_file('loads.dyn', loads)
 system('ls-dyna-d ncpu=%s i=%s' % (NTASKS, DYNADECK))
