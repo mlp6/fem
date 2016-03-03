6763bef3c41376e8f85a1d7868a2ec0707cac67a
Mark Palmeri <mlp6@duke.edu>
2016-03-03 18:39:09 -0500

    consolidate gaussian excitation module call

--- examples/gauss_qsym_pml/run.py
+++ examples/gauss_qsym_pml/run.py
@@ -1,15 +1,15 @@
 #!/bin/env python
-#SBATCH --mem=2G
+#SBATCH --mem=4G
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
-##SBATCH --mail-user=email@address.edu
-##SBATCH --mail-type=END
+#SBATCH --ntasks-per-node=8
+#SBATCH --mail-user=mlp6@duke.edu
+#SBATCH --mail-type=END
 
 from time import ctime
 from os import environ, system
 from socket import gethostname
-from gen_mesh_bc import main as gen_mesh_bc
-from fem.mesh import GaussExc
+from fem.mesh import GenMesh, bc
+from fem.mesh.GaussExc import generate_loads
 from fem.post.create_disp_dat import create_dat as create_disp_dat
 from fem.post.create_res_sim_mat import run as create_res_sim_mat
 
@@ -19,11 +19,24 @@ print('HOST: %s' % gethostname())
 DYNADECK='gauss_qsym_pml.dyn'
 NTASKS = environ.get('SLURM_NTASKS', '8')
 
-gen_mesh_bc()
-loads = GaussExc.read_process_nodes([0.25,0.25,0.75], [0.0,0.0,-1.5],)
-GaussExc.write_load_file('loads.dyn', loads)
+xyz = (-1.5, 0.0, 0.0, 1.5, -3.0, 0.0)
+numElem = (75, 75, 150)
+GenMesh.run(xyz, numElem)
+
+# setup quarter symmetry condition
+pml_elems = ((5, 0), (0, 5), (5, 5))
+face_constraints = (('1,1,1,1,1,1', '1,0,0,0,1,1'),
+                    ('0,1,0,1,0,1', '1,1,1,1,1,1'),
+                    ('1,1,1,1,1,1', '1,1,1,1,1,1'))
+edge_constraints = (((0,1),(1,0),(0,0)),'1,1,0,1,1,1')
+bc.apply_pml(pml_elems, face_constraints, edge_constraints)
+
+generate_loads([0.25,0.25,0.75], [0.0,0.0,-1.5])
+
 system('ls-dyna-d ncpu=%s i=%s' % (NTASKS, DYNADECK))
+
 create_disp_dat()
+
 create_res_sim_mat()
 
 #if [ -e res_sim.mat ];
