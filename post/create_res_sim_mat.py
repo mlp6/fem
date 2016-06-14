ce5e8cc315674da11c292de336904ff8b56b8ec3
Mark Palmeri <mlp6@duke.edu>
2016-06-14 16:51:44 -0400

    add note about flipud to accomodate C- vs. F-ordering of ndenumerate

--- post/create_res_sim_mat.py
+++ post/create_res_sim_mat.py
@@ -113,6 +113,8 @@ def extract_arfi_data(dispout, header, image_plane, disp_comp=2, disp_scale=-1e4
     print('done!')
     fid.close()
 
+    # ndenumerate only iterates in C-ordering, so flip this over to match the
+    # F-ordering of Matlab-like code
     arfidata = np.flipud(arfidata)
 
     return arfidata
