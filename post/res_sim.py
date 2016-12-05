be0278f718235d88b8587bbc2216742d6e7a745c
Mark Palmeri <mlp6@duke.edu>
2016-12-05 15:53:51 -0500

    load in dict keys as obj attrs

--- post/res_sim.py
+++ post/res_sim.py
@@ -1,30 +1,24 @@
-class ResSim:
+class Res:
     """plot and animate res_sim.mat simulation data
     """
-    def __init__(self):
-        pass
+    def __init__(self, filename="res_sim.mat"):
+        self.load(filename)
 
 
-    def __str__():
-        pass
-
-    def load(self, filename='res_sim.mat'):
+    def load(self, filename):
         """load MATv5 data
 
-        Creates 'res' dict containing:
-        * lat
-        * axial
-        * t
-        * arfidata (3D)
-
         :param filename:
+        :returns: attributes - lat, axial, t, arfidata
         """
         from scipy.io import loadmat
 
-        self.res = loadmat(filename)
-        print(self.res.keys())
-
-        return
+        d = loadmat(filename)
+        
+        self.lat = d['lat']
+        self.axial = d['axial'].transpose()
+        self.t = d['t']
+        self.arfidata = d['arfidata']
 
 
     def plot(self, timestep):
@@ -34,11 +28,11 @@ class ResSim:
         """
         import matplotlib.pyplot as plt
 
-        plt.pcolormesh(self.res['lat'], self.res['axial'].transpose(), self.res['arfidata'][:, :, timestep])
+        plt.pcolormesh(self.lat, self.axial, self.arfidata[:, :, timestep])
         plt.axes().set_aspect('equal')
         plt.xlabel('Lateral (mm)')
         plt.ylabel('Axial (mm)')
-        plt.title('t = {:.2f} ms'.format(self.res['t'][0, timestep]*1e3))
+        plt.title('t = {:.2f} ms'.format(self.t[0, timestep]*1e3))
         plt.gca().invert_yaxis()
         plt.show()
 
