366f42714eeae55957bb4b72675177196a564bb1
Mark Palmeri <mlp6@duke.edu>
2016-12-04 23:04:34 -0500

    WIP: plot works, animate does not (scrapped attempt at class for now)

--- post/res_sim.py
+++ post/res_sim.py
@@ -1,45 +1,49 @@
-class ResSim:
-    """plot and animate res_sim.mat simulation data
+res = {}
+import matplotlib.pyplot as plt
+fig = plt.figure()
+
+def load(filename="res_sim.mat"):
+    """load MATv5 data
+
+    :param filename:
+    :returns: res (dict: lat, axial, t, arfidata)
     """
-    def __init__(self):
-        pass
+    from scipy.io import loadmat
+
+    res = loadmat(filename)
+
+    return res
 
 
-    def __str__():
-        pass
+def plot(res, timestep):
+    """plot arfidata at specified timestep
 
-    def load(self, filename='res_sim.mat'):
-        """load MATv5 data
+    :param resdict: res_sim.mat dict
+    :param timestep: int
+    """
 
-        Creates 'res' dict containing:
-        * lat
-        * axial
-        * t
-        * arfidata (3D)
-
-        :param filename:
-        """
-        from scipy.io import loadmat
-
-        self.res = loadmat(filename)
-        print(self.res.keys())
-
-        return
+    plt.axes().set_aspect('equal')
+    plt.xlabel('Lateral (mm)')
+    plt.ylabel('Axial (mm)')
+    plt.gca().invert_yaxis()
+    plt.title('{:.2f} ms'.format(res['t'][0, timestep]*1e3))
+    plt.pcolormesh(res['lat'], res['axial'].transpose(), res['arfidata'][:, :, timestep])
+    plt.show()
 
 
-    def plot(self, timestep):
-        """plot arfidata at specified timestep
+def play(res, frames):
+    """animate plots
 
-        :param timestep: int
-        """
-        import matplotlib.pyplot as plt
+    :param frames: range of timesteps to animate
+    """
+    from matplotlib.animation import FuncAnimation
 
-        plt.pcolormesh(self.res['lat'], self.res['axial'].transpose(), self.res['arfidata'][:, :, timestep])
-        plt.axes().set_aspect('equal')
-        plt.xlabel('Lateral (mm)')
-        plt.ylabel('Axial (mm)')
-        plt.title('t = {:.2f} ms'.format(self.res['t'][0, timestep]*1e3))
-        plt.gca().invert_yaxis()
-        plt.show()
+    a = FuncAnimation(fig, animate, frames=frames, blit=False)
+    plt.show()
 
-        return
+
+def animate(t):
+    plt.title('{:.2f} ms'.format(res['t'][0, timestep]*1e3))
+    p = plt.pcolormesh(res['lat'], res['axial'].transpose(), res['arfidata'][:, :, timestep])
+
+    return p
