81bdac2aa1be99448bac9c1d023c694e85985c8c
Mark Palmeri <mlp6@duke.edu>
2017-01-23 21:30:23 -0500

    ADD more unit test coverage

--- tests/test_TopLoad.py
+++ tests/test_TopLoad.py
@@ -15,9 +15,10 @@ def test_extract_top_plane_nodes():
     (direction, planeNodeIDs) = extract_top_plane_nodes(nodefile=nodefile, top_face=[0, 0, 0, 0, 0, 1])
 
     assert direction == 3
-    #TODO: test planeNodeIDs
+    assert planeNodeIDs[0][0] == 1211
+    assert planeNodeIDs[-1][-1] == 1331
 
-def test_writeNodeLoads(tmpdir):
+def test_writeNodeLoads_disp(tmpdir):
     from TopLoad import writeNodeLoads
     f = tmpdir.join("top_load.dyn")
     writeNodeLoads(loadfile=f.strpath, planeNodeIDs=[[1, 2 ,3], [4, 5, 6]], loadtype='disp', direction=3,
@@ -27,6 +28,15 @@ def test_writeNodeLoads(tmpdir):
     assert lines[1] == "1,3,2,1,-1.000000\n"
     assert lines[-1] == "*END\n"
 
+def test_writeNodeLoads_force(tmpdir):
+    from TopLoad import writeNodeLoads
+    f = tmpdir.join("top_load.dyn")
+    writeNodeLoads(loadfile=f.strpath, planeNodeIDs=[[1, 2 ,3], [4, 5, 6]], loadtype='force', direction=3,
+                   amplitude=-1.0, lcid=1)
+    lines = f.readlines()
+    assert lines[0] == "*LOAD_NODE_POINT\n"
+    assert lines[1] == "1,3,1,-1.000000\n"
+    assert lines[-1] == "*END\n"
 
 def test_read_cli():
     from TopLoad import read_cli
