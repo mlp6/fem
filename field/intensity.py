ecc91ef6ed67c5c9e776c4ff01654ecd8a51f2ae
Mark Palmeri <mark.palmeri@duke.edu>
2018-02-01 23:32:15 -0500

    convert field intensities to point loads (#24)
    * add Intensity class
    
    * add PointLoads class
    
    * ignore virtualenv env directory

---
+++ field/intensity.py
@@ -0,0 +1,42 @@
+class Intensity:
+    def __init__(self, dynamat, nodefile='nodes.dyn'):
+        self.dynamat = dynamat
+        self.nodefile = nodefile
+        self.FIELD_PARAMS = None
+        self.intensity = None
+        self.focal_depth_cm = None
+        self.load_intensity()
+        self.load_sorted_nodes()
+
+    def load_intensity(self):
+        from scipy.io import loadmat
+
+        field_intensities = loadmat(self.dynamat)
+        self.intensity = field_intensities['intensity']
+        self.FIELD_PARAMS = field_intensities['FIELD_PARAMS']
+        # self.focal_depth_cm = self.FIELD_PARAMS['focus'][2] * 100
+
+    def load_sorted_nodes(self):
+        from fem.mesh import fem_mesh
+
+        nic = fem_mesh.load_nodeIDs_coords(nodefile=self.nodefile)
+        [self.snic, self.axes] = fem_mesh.SortNodeIDs(nic)
+
+    def show_image_plane(self, ele_coord=0):
+        from fem.mesh import fem_mesh
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        # -1 on next to move to 0 start indexing
+        planeNodeIDs = fem_mesh.extractPlane(self.snic, self.axes,
+                                             (0, ele_coord)) - 1
+
+        image_plane_intensity = np.take(self.intensity, planeNodeIDs)
+
+        plt.imshow(np.flipud(image_plane_intensity.transpose()))
+        plt.show()
+
+    def gen_pt_loads(self, node_search_tol_cm=1e-3):
+        """generate ls-dyna point loads input file
+        """
+        pass
---
+++ field/pointloads.py
@@ -0,0 +1,45 @@
+class PointLoads:
+    def __init__(self, loadfile, nodefile='nodes.dyn'):
+        self.loadfile = loadfile
+        self.nodefile = nodefile
+        self.loads = None
+        self.load_loads()
+        self.load_sorted_nodes()
+
+    def load_loads(self):
+        import numpy as np
+
+        self.pt_loads = np.loadtxt('PointLoads-f1.80-F1.0-FD0.120-a0.50.dyn',
+                                   comments=['$', '*'],
+                                   delimiter=',',
+                                   dtype={'names': ('NID', 'Direction', 'LCID',
+                                                    'Magnitude', 'None'),
+                                          'formats': ('i4', 'i4', 'i4',
+                                                      'f4', 'i4')})
+
+    def load_sorted_nodes(self):
+        from fem.mesh import fem_mesh
+
+        nic = fem_mesh.load_nodeIDs_coords(nodefile=self.nodefile)
+        [self.snic, self.axes] = fem_mesh.SortNodeIDs(nic)
+
+    def show_image_plane(self, ele_coord=0):
+        from fem.mesh import fem_mesh
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        # -1 on next to move to 0 start indexing
+        planeNodeIDs = fem_mesh.extractPlane(self.snic, self.axes,
+                                             (0, ele_coord))
+
+        image_plane_loads = np.zeros(planeNodeIDs.shape)
+        for m, a in enumerate(planeNodeIDs):
+            for n, nid in enumerate(a):
+                b = np.where(self.pt_loads['NID'] == nid)
+                try:
+                    image_plane_loads[m][n] = self.pt_loads['Magnitude'][b[0]]
+                except:
+                    pass
+
+        plt.imshow(np.flipud(image_plane_loads.transpose()))
+        plt.show()
