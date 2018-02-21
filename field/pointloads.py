2d0c84dfa8453ce7528a1130a8ed944b77ec0d26
Mark Palmeri <mlp6@duke.edu>
2018-02-21 00:00:43 -0500

    [wip] add some structure for the 3D quiver plot

--- field/pointloads.py
+++ field/pointloads.py
@@ -1,5 +1,5 @@
 class PointLoads:
-    def __init__(self, loadfile, nodefile='nodes.dyn'):
+    def __init__(self, loadfile=None, nodefile='nodes.dyn'):
         self.loadfile = loadfile
         self.nodefile = nodefile
         self.loads = None
@@ -7,9 +7,11 @@ class PointLoads:
         self.load_sorted_nodes()
 
     def load_loads(self):
+        """load point loads from loadfile
+        """
         import numpy as np
 
-        self.pt_loads = np.loadtxt('PointLoads-f1.80-F1.0-FD0.120-a0.50.dyn',
+        self.pt_loads = np.loadtxt(self.loadfile,
                                    comments=['$', '*'],
                                    delimiter=',',
                                    dtype={'names': ('NID', 'Direction', 'LCID',
@@ -18,12 +20,18 @@ class PointLoads:
                                                       'f4', 'i4')})
 
     def load_sorted_nodes(self):
+        """load-n-sort mesh nodes
+        """
         from fem.mesh import fem_mesh
 
         nic = fem_mesh.load_nodeIDs_coords(nodefile=self.nodefile)
         [self.snic, self.axes] = fem_mesh.SortNodeIDs(nic)
 
     def show_image_plane(self, ele_coord=0):
+        """show an image of the loads on a plane
+
+        :param ele_coord=0: elevation position of the plane
+        """
         from fem.mesh import fem_mesh
         import numpy as np
         import matplotlib.pyplot as plt
@@ -36,6 +44,7 @@ class PointLoads:
         for m, a in enumerate(planeNodeIDs):
             for n, nid in enumerate(a):
                 b = np.where(self.pt_loads['NID'] == nid)
+                #TODO: Handle the direction of the push
                 try:
                     image_plane_loads[m][n] = self.pt_loads['Magnitude'][b[0]]
                 except:
@@ -43,3 +52,26 @@ class PointLoads:
 
         plt.imshow(np.flipud(image_plane_loads.transpose()))
         plt.show()
+
+    def plot3Dquiver(self):
+        """generate 3D quiver plot of all loads
+        """
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import axes3d
+        import numpy as np
+
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+
+        dx = np.mean(np.diff(axes[0]))
+        dy = np.mean(np.diff(axes[1]))
+        dz = np.mean(np.diff(axes[2]))
+        X, Y, Z = np.meshgrid(np.arange(self.axes[0][0], self.axes[0][-1], dx),
+                              np.arange(self.axes[1][0], self.axes[1][-1], dy),
+                              np.arange(self.axes[2][0], self.axes[2][-1], dz))
+
+        #TODO: map load vectors to meshgrid locations
+
+        # https://matplotlib.org/mpl_toolkits/mplot3d/tutorial.html#quiver
+        ax.quiver(X, Y, Z, U, V, W, length=0.1, normalize=True)
+
