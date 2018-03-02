41d4f80ccd041c2dbf3e52323e25862c6dfdb171
Mark Palmeri <mlp6@duke.edu>
2018-03-01 21:46:04 -0500

    refactor vector save method

--- post/savevtk.py
+++ post/savevtk.py
@@ -12,21 +12,21 @@ class SaveVTK:
     def __init__(self, data, origin, spacing):
         """ initialize the object
 
-        :param data: 3D data
-        :param origin: (x, y, z)
-        :param spacing: (x, y, z)
+        :param float data: 3D numpy array [scalar]
+                           3D numpy array object (X, Y, Z attributes) [vector]
+        :param float origin: mesh origin tuple (x, y, z)
+        :param float spacing: node spacing tuple (x, y, z)
         """
         self.data = data
         self.origin = origin
         self.spacing = spacing
 
 
-    def save_scalar(self, filename, data_name="scalars", header_comment=None):
+    def save_scalar(self, filename, header_comment=None):
         """save 3D scalar data in VTK structure points format
 
-        :param filename:
-        :param data_name:
-        :param header_comment: default = None
+        :param string filename: output filename
+        :param string header_comment: default = None
         """
         import numpy as np
 
@@ -36,11 +36,11 @@ class SaveVTK:
                 vtkfile.write("%s\n" % (header_comment))
             vtkfile.write("ASCII\n\n")
             vtkfile.write("DATASET STRUCTURED_POINTS\n")
-            vtkfile.write("DIMENSIONS    %d   %d   %d\n\n" % (self.data.shape))
-            vtkfile.write("ORIGIN    %.2f   %.2f   %.2f\n" % (self.origin))
-            vtkfile.write("SPACING    %.3f   %.3f   %.3f\n\n" % (self.spacing))
-            vtkfile.write("POINT_DATA   %d\n" % (np.prod(self.data.shape)))
-            vtkfile.write("SCALARS %s float\n" % data_name)
+            vtkfile.write("DIMENSIONS    {:d}   {:d}   {:d}\n\n".format(*self.data.shape))
+            vtkfile.write("ORIGIN    {:.2f}   {:.2f}   {:.2f}\n".format(*self.origin))
+            vtkfile.write("SPACING    {:.3f}   {.3f}   {.3f}\n\n".format(*self.spacing))
+            vtkfile.write("POINT_DATA   {:d}\n".format(np.prod(self.data.shape)))
+            vtkfile.write("SCALARS scalars float\n")
             vtkfile.write("LOOKUP_TABLE default\n\n")
             for x in range(0, self.data.shape[0]):
                 for y in range(0, self.data.shape[1]):
@@ -48,4 +48,39 @@ class SaveVTK:
                         vtkfile.write("%.6e " % self.data[x, y, z])
                     vtkfile.write(" \n")
 
-        return 0
+
+    def save_vector(self, filename, header_comment=None):
+        """save 3D vector array in VTK structure points format
+
+        :param string filename: output filname
+        :param string header_comment: default = None
+        """
+        import numpy as np
+
+        # TODO: move this check when initializing the data object
+        if ((size(X) != size(Y)) | (size(X) != size(Z))):
+            fprint('Error: velocity arrays of unequal size\n'); return;
+        end
+
+        with open(filename, 'w') as vtkfile:
+            vtkfile.write('# vtk DataFile Version 2.0\n')
+            if header_comment:
+                vtkfile.write("%s\n" % (header_comment))
+            vtkfile.write('ASCII\n')
+            vtkfile.write('\n')
+            vtkfile.write('DATASET STRUCTURED_POINTS\n')
+            vtkfile.write('DIMENSIONS    {:d}   {:d}   {:d}\n'.format(data.X.shape))
+            vtkfile.write('\n');
+            vtkfile.write("ORIGIN    {:.2f}   {:.2f}   {:.2f}\n".format(*self.origin))
+            vtkfile.write("SPACING   {:.3f}   {:.3f}   {:.3f}\n\n".format(*self.spacing))
+            vtkfile.write('\n')
+            vtkfile.write('POINT_DATA   {:d}\n'.format(np.prod(dims)))
+            vtkfile.write('VECTORS vectors float\n')
+            vtkfile.write('\n')
+            for a in range(0, self.data.X.shape[0]):
+                for b in range(0, self.data.X.shape[1]):
+                    for c in range(0, self.data.X.shape[2]):
+                        vtkfile.write('%f ', self.data.X[a, b, c])
+                        vtkfile.write('%f ', self.data.Y[a, b, c])
+                        vtkfile.write('%f ', self.data.Z[a, b, c])
+                    vtkfile.write('\n')
