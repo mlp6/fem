b185a3f900a67094ab4712cf9e938af82074cce6
Mark Palmeri <mlp6@duke.edu>
2018-03-04 00:52:50 -0500

    change _build/ to build/ in docs to try to work with GitHub pages

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
+    'sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -72,7 +73,7 @@ release = '6.9.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
