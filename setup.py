952ffa88ce24c17d5ebc5c3d69db69e5debb38ac
Mark Palmeri <mlp6@duke.edu>
2018-03-04 11:47:35 -0500

    Revert "try using package_dir"
    This reverts commit 426be1c71d3b014dadd45870481776d40026c72a.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info.major != 3 or (sys.version_info.major == 3 and
 
 setup(
     name='fem',
-    package_dir=['.'],
+    packages=['.', 'field', 'mesh', 'post'],
     version='6.9.1',
     license='Apache v2.0',
     author='Mark Palmeri',
