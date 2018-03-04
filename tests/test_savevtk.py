89f4d1668de685453c3edeff21a7508fbad80033
Mark Palmeri <mlp6@duke.edu>
2018-03-03 21:06:52 -0500

    fix bugs; add written vector file assertions

--- tests/test_savevtk.py
+++ tests/test_savevtk.py
@@ -66,7 +66,7 @@ def test_savevtk_raise_3D_exception(savevtk_data):
 def test_write_scalar(savevtk_data, tmpdir):
     from savevtk import SaveVTK
 
-    fname = tmpdir.join('file.vtk')
+    fname = tmpdir.join('scalar.vtk')
 
     vtkobj = SaveVTK(savevtk_data, (0.0, 1.0, 2.0), (0.1, 0.2, 0.3))
 
@@ -75,7 +75,36 @@ def test_write_scalar(savevtk_data, tmpdir):
     with open(fname.strpath, 'r') as written_file:
         text = written_file.readlines()
 
+    assert text[1] == 'unit test\n'
     assert text[2] == 'ASCII\n'
     assert text[5].split()[2] == '5'
     assert text[7].startswith('ORIGIN')
     assert text[8].split()[3] == '0.300'
+    assert text[10].split()[1] == '120'
+    assert text[20].split()[0] == '4.000000e+00'
+    assert text[32].split()[3] == '1.600000e+01'
+
+
+def test_write_vector(savevtk_data, tmpdir):
+    from savevtk import SaveVTK
+
+    fname = tmpdir.join('vector.vtk')
+
+    data3d = (savevtk_data, savevtk_data, savevtk_data)
+
+    vtkobj = SaveVTK(data3d, (0.0, 1.0, 2.0), (0.1, 0.2, 0.3))
+
+    vtkobj.save_vector(fname.strpath, dataname="vectors", header_comment="vector unit test")
+
+    with open(fname.strpath, 'r') as written_file:
+        text = written_file.readlines()
+
+    assert text[1] == 'vector unit test\n'
+    assert text[2] == 'ASCII\n'
+    assert text[5].split()[2] == '5'
+    assert text[7].startswith('ORIGIN')
+    assert text[8].split()[3] == '0.300'
+    assert text[10].split()[1] == '120'
+    assert text[29].split()[17] == '8.000000'
+    assert text[32].split()[17] == '20.000000'
+
