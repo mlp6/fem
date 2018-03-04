4126f2e60064832f1c816bdb8fc3c7f9e9066a94
Mark Palmeri <mlp6@duke.edu>
2018-03-03 19:49:36 -0500

    add nparray tests with savevtk_data fixture

--- tests/test_savevtk.py
+++ tests/test_savevtk.py
@@ -4,16 +4,19 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(myPath, '../post/'))
 
 
-def test_savevtk_data_nparray():
-    """test that nparray is read in correctly
+def test_savevtk_data_nparray(savevtk_data):
+    """test that nparray data read in correctly
     """
+    from numpy import ndarray
+
+    assert isinstance(savevtk_data, ndarray)
+    assert savevtk_data[0, 0, 0] == 1.0
+    assert savevtk_data[3, 4, 5] == 20.0
+
+
+def test_savevtk_data_nparray_attribute(savevtk_data):
     from savevtk import SaveVTK
-    from scipy.io import loadmat
-    import numpy as np
 
-    d = loadmat('{}/savevtk_data.mat'.format(myPath))
-    data = d['data']
+    vtkobj = SaveVTK(savevtk_data, (0.0, 0.0, 0.0), (0.1, 0.1, 0.1))
 
-    assert isinstance(data, np.ndarray)
-    assert data[0, 0, 0] == 1.0
-    assert data[3, 4, 5] == 20.0
+    assert savevtk_data.all() == vtkobj.data.all()
