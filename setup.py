5fd834502eecd0cc8c64b9b947859b83a6e35fd6
Mark Palmeri <mlp6@duke.edu>
2018-03-04 11:23:06 -0500

    allow python3.5 to be used

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 import sys
 
 if sys.version_info.major != 3 or (sys.version_info.major == 3 and
-                                   sys.version_info.minor < 6):
-    sys.exit('You must use python >=3.6')
+                                   sys.version_info.minor < 5):
+    sys.exit('You must use python >=3.5')
 
 setup(
     name='fem',
