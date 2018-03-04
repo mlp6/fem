f128a6896714ff45af6859c5cac6b60c5dd1c162
Mark Palmeri <mlp6@duke.edu>
2018-03-04 11:25:04 -0500

    update setup.py for pip install

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ if sys.version_info.major != 3 or (sys.version_info.major == 3 and
 setup(
     name='fem',
     packages=['fem', ],
-    version='6.8.2',
+    version='6.9.1',
     license='Apache v2.0',
     author='Mark Palmeri',
     author_email='mlp6@duke.edu',
-    description='Acoustic Radiation Force FEM Tools',
+    description='FEM',
     keywords=['arfi', 'swei', 'fem'],
     long_description=open('README.md').read(),
     url='https://github.com/mlp6/fem',
-    download_url='https://github.com/mlp6/fem/archive/v6.8.2.tar.gz',
+    download_url='https://github.com/mlp6/fem/archive/v6.9.1.tar.gz',
     classifiers=[],
 )
