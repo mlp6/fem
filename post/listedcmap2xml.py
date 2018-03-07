b0ffbfceea0342d3e70e4017b482483418ba23d7
Mark Palmeri <mlp6@duke.edu>
2018-03-07 13:45:44 -0500

    add a colormap that matches Matlab Parula to render similar-looking plots/images

---
+++ post/listedcmap2xml.py
@@ -0,0 +1,14 @@
+def listedcmap2xml(cmap):
+    """Convert ListedColormap to XML format for import into Paraview
+
+    Based on: https://www.paraview.org/Wiki/Colormaps
+
+    Args:
+        ListedColormap (ListedColormap): Matplotlib-formated ListedColormap
+    """
+    N = len(cmap.colors);
+    with open('{}.xml'.format(cmap.name), 'w') as f:
+        f.write('<ColorMap name="{}" space="HSV">\n'.format(cmap.name))
+        for n, i in enumerate(cmap.colors):
+             f.write('  <Point x="{:f}" o="1" r="{:f}" g="{:f}" b="{:f}"/>\n'.format(((n)/(N-1)), *i))
+        f.write('</ColorMap>')
