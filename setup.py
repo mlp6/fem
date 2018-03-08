05c1d35d1655a08320978769534dfb6993ab49e8
Mark Palmeri <mark.palmeri@duke.edu>
2018-03-07 21:51:18 -0500

    expand python version compatibility (#39)
    * add more python versions (3>=3.4) to CI testing
    
    * update CI testing to just C++ compile create_disp_dat.c (no SWIG compilation)
    
    * upate Makefiles and Travis C compilation tests
    
    * Relax Python version requirement to >= 3.4.
    
    * expand test to dev versions
    
    * rm 3.4-dev (not available) and 3.7-dev (opensll not available)

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 import sys
 
 if sys.version_info.major != 3 or (sys.version_info.major == 3 and
-                                   sys.version_info.minor < 5):
-    sys.exit('You must use python >=3.5')
+                                   sys.version_info.minor < 4):
+    sys.exit('You must use python >=3.4')
 
 setup(
     name='fem',
     packages=['fem', 'fem.mesh', 'fem.post', 'fem.field'],
     package_dir={'fem': '.'},
-    version='7.0.0',
+    version='7.0.2',
     license='Apache v2.0',
     author='Mark Palmeri',
     author_email='mlp6@duke.edu',
@@ -17,7 +17,7 @@ setup(
     keywords=['arfi', 'swei', 'fem', 'ultrasound'],
     long_description=open('README.md').read(),
     url='https://github.com/mlp6/fem',
-    download_url='https://github.com/mlp6/fem/archive/v7.0.0.tar.gz',
+    download_url='https://github.com/mlp6/fem/archive/v7.0.2.tar.gz',
     classifiers=[],
     install_requires=['h5py', 'ipython', 'jupyter', 'matplotlib', 'numpy',
                       'pytest', 'pytest-pep8', 'scipy', 'sphinx',
