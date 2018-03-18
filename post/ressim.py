d2bfd181c9194862a179c68537af39798cede94e
Mark Palmeri <mlp6@duke.edu>
2018-03-18 11:54:57 -0400

    add more method arguments to customize output

--- post/ressim.py
+++ post/ressim.py
@@ -23,7 +23,8 @@ class ResSim:
         self.t = d['t'].squeeze() * 1e3  # convert from s -> ms
         self.arfidata = d['arfidata']
 
-    def plot(self, timestep, show=True, save=False, savename='file'):
+    def plot(self, timestep, show=True, save=False, savename='file', xlabel='Lateral (mm)',
+             ylabel='Axial (mm)', title=None):
         """Plot arfidata at specified timestep.
 
         Args:
@@ -31,6 +32,9 @@ class ResSim:
             show (Boolean): show plot
             save (Boolean): save PNG (objectname_timestep.png)
             savename (str): saved PNG filename prefix
+            xlabel (str):
+            ylabel (str):
+            title (str):
 
         Returns:
 
@@ -39,9 +43,12 @@ class ResSim:
 
         plt.pcolormesh(self.lat, self.axial, self.arfidata[:, :, timestep])
         plt.axes().set_aspect('equal')
-        plt.xlabel('Lateral (mm)')
-        plt.ylabel('Axial (mm)')
-        plt.title('t = {:.2f} ms'.format(self.t[timestep]))
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        if title is None:
+            plt.title('t = {:.2f} ms'.format(self.t[timestep]))
+        else:
+            plt.title(title)
         plt.gca().invert_yaxis()
         if save:
             filename = '{}_{:04d}.png'.format(savename, timestep)
@@ -50,14 +57,17 @@ class ResSim:
         if show:
             plt.show()
 
-        return
-
-    def timeplot(self, axial, lat):
+    def timeplot(self, axial, lat, xlabel='Time (ms)', ylabel='Displacement (\mum)', title=None,
+                 show=True):
         """plot arfidata through time at specified ax and lat position (mm)
 
         Args:
             axial (ndarray): axial depth (mm)
             lat (ndarray): lateral position (mm)
+            xlabel (str):
+            ylabel (str):
+            title (str):
+            show (Boolean): show the plot
 
         Returns:
 
@@ -68,23 +78,32 @@ class ResSim:
         axInd = np.min(np.where(self.axial >= axial))
         latInd = np.min(np.where(self.lat >= lat))
         plt.plot(self.t, self.arfidata[axInd, latInd, :])
-        plt.xlabel('Time (ms)')
-        plt.ylabel('Displacement (\mum)')
-        plt.title('Axial = {:.1f} mm, Lateral = {:.1f} mm'.
-                  format(self.axial[axInd], self.lat[latInd]))
-        plt.show()
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        if title is None:
+            plt.title('Axial = {:.1f} mm, Lateral = {:.1f} mm'.
+                      format(self.axial[axInd], self.lat[latInd]))
+        else:
+            plt.title=(title)
 
-        return
+        if show:
+            plt.show()
 
-    def play(self, timerange):
+    def play(self, timerange, xlabel='Lateral (mm)', ylabel='Axial (mm)', show=True):
         """play an animation
 
         Strongly recommend not stepping though each timesteps; use some skips!
 
         Args:
             timerange (range): range generator of time steps to animate
+            xlabel (str):
+            ylabel (str):
+            show (Boolean): show animation
 
         Returns:
+
+        Todo:
+            * Add save option
 
         """
         import matplotlib.pyplot as plt
@@ -95,13 +114,14 @@ class ResSim:
         plt.pcolormesh(self.lat, self.axial, self.arfidata[:, :, 0])
         plt.axes().set_aspect('equal')
         plt.gca().invert_yaxis()
-        plt.xlabel('Lateral (mm)')
-        plt.ylabel('Axial (mm)')
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
 
         anim = animation.FuncAnimation(fig, self.animate, frames=timerange,
                                        blit=False)
 
-        plt.show()
+        if show:
+            plt.show()
 
     def animate(self, i):
         import matplotlib.pyplot as plt
