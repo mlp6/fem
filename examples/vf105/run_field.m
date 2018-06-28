dc271f1b056bd39242b42f841b09aaaa4209d399
Mark Palmeri <mlp6@duke.edu>
2018-06-27 22:28:02 -0400

    start to update example

---
+++ examples/vf105/run_field.m
@@ -0,0 +1,3 @@
+addpath('/home/mlp6/Documents/fem/field/'); 
+addpath('/home/mlp6/Documents/MATLAB/Field_II_Pro/m_files'); 
+field2dyna('nodes.dyn', 0.5, 1.0, [0.0 0.0 0.02], 7.2, 'vf105', 'gaussian', 1, true);"
