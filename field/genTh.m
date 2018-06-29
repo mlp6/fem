7967f7dd49f0592c6266c2fb158fbadde24d8355
Mark Palmeri <mlp6@duke.edu>
2018-06-29 12:43:04 -0400

    change Th definition to be based on probe.type, not probe.name

--- field/genTh.m
+++ field/genTh.m
@@ -11,25 +11,25 @@ function Th = genTh(probe)
 %     Th - pointer to Field II transducer definition
 %
 
-switch probe.name
-    case {'vf105', 'vf135', 'vf73', 'vf105gfp', 'sonivate', 'tu15l8w', 'l74', ...
-          'l94', 'l124', 'l145', 'i7505', 'acunav64', 'acunav128', ...
-          'acunav128_fullapp', 'er7bl', 'p42', 'ph41', 'pl35elegra'}
+switch probe.type
+    case 'focused_multirow'
         Th = xdc_focused_multirow(probe.no_elements, probe.width,
                                   probe.no_elements_y, probe.height, ... 
                                   probe.kerf, probe.kerf, probe.Rfocus, ...
                                   probe.no_sub_x, probe.no_sub_y, ...
                                   FIELD_PARAMS.focus);
-    case {'c52', 'c52v', 'ch41', 'ch62', 'ev94'}
+    case 'convex_focused_array'
         Th = xdc_convex_focused_array(probe.no_elements, probe.width, probe.height, ...
                                       probe.kerf, probe.Rconvex, probe.Rfocus, ...
                                       probe.no_sub_x, probe.no_sub_y, ...
                                       FIELD_PARAMS.focus);
-    case {'hifu', 'drug_piston'}
+    case 'concave'
+        %case {'hifu', 'drug_piston'}
         Th = xdc_concave(probe.R, probe.Rfocal, probe.ele_size);
-    case {'v41c'}
+    case 'focused_array'
+        %case {'v41c'}
         Th = xdc_focused_array(probe.no_elements, probe.width, probe.height, ...
                                probe.kerf, probe.Rfocus, probe.no_sub_x, ...
                                probe.no_sub_y, FIELD_PARAMS.focus);
     otherwise
-        warning('Specified probe not explicitly handled.')
+        warning('Specified probe type not explicitly handled.')
