842df2fe0763192fde44e9dc99fd3092725fbf87
Mark Palmeri <mlp6@duke.edu>
2018-06-29 13:54:35 -0400

    clean up JSON keys; fix matlab syntax spacing

--- field/defineImpResp.m
+++ field/defineImpResp.m
@@ -1,5 +1,6 @@
-function [impulseResponse]=defineImpResp(fBW,Fc,FIELD_PARAMS);
-% function [impulseResponse]=defineImpResp(fBW,Fc,FIELD_PARAMS);
+function [impulseResponse]=defineImpResp(fBW, Fc, FIELD_PARAMS);
+% function [impulseResponse]=defineImpResp(fBW, Fc, FIELD_PARAMS);
+%
 % Define the impulse reponse of the array, either using a Gaussian or
 % experimentally-measured data.
 %
@@ -12,15 +13,14 @@ function [impulseResponse]=defineImpResp(fBW,Fc,FIELD_PARAMS);
 %   impulseResponse (float vector) - impulse response at the specified sampling
 %   frequency
 %
-% Mark 06/20/07
 
 switch FIELD_PARAMS.impulse
     case 'gaussian',
         disp('Impulse Response: Gaussian');
-        tc = gauspuls('cutoff',Fc,fBW,-6,-40);
+        tc = gauspuls('cutoff', Fc, fBW, -6, -40);
         t = -tc:1/FIELD_PARAMS.sampling_freq_Hz:tc;
-        impulseResponse =gauspuls(t,Fc,fBW);
+        impulseResponse = gauspuls(t, Fc, fBW);
     case 'exp',
         disp('Impulse Response: Experimental');
-        [impulseResponse]=formatExpImpResp(FIELD_PARAMS);
+        [impulseResponse] = formatExpImpResp(FIELD_PARAMS);
 end;
