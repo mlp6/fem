091ae4a538845eea76401eb162f8118bab216b14
Mark Palmeri <mlp6@duke.edu>
2018-07-02 12:28:31 -0400

    add temp script to generate point loads file

---
+++ examples/vf105/mk.m
@@ -0,0 +1,4 @@
+addpath('/home/mlp6/fem/field'); 
+addpath('/home/mlp6/Documents/MATLAB/Field_II_Pro/m_files'); 
+makeLoadsTemps('dyna-I-f7.20-F1.0-FD0.020-a0.50.mat', 'dyna-I-f7.20-F1.0-FD0.020-a0.50.mat', 1000,400,4.2,0.01^3,'q',1); 
+quit;
