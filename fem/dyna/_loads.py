Fixed speed of sound units

fem/dyna/_loads.py
@@ -117,6 +117,8 @@ class DynaMeshLoadsMixin:
 
         nlat, nele, nax = len(extent['lat']), len(extent['ele']), len(extent['ax'])
         intensity = mat['intensity'].reshape(nax, nlat, nele)
+
+        # Swap to dyna coord system (ele, lat, -ax)
         intensity = np.swapaxes(intensity, 0, 2)
         intensity = np.flip(intensity, axis=2)
 
@@ -167,10 +169,13 @@ class DynaMeshLoadsMixin:
     
     def add_field_arf_load(self, field_load_file, normalization_isppa, load_curve_id):
         mat = loadmat(field_load_file)
-        c = mat['FIELD_PARAMS']['soundSpeed'][0,0][0][0]*100 # convert from m/s to cm/s
+        # c = mat['FIELD_PARAMS']['soundSpeed'][0,0][0][0]*100 # convert from m/s to cm/s
+        c = mat['FIELD_PARAMS']['soundSpeed'][0,0][0][0]
         alpha_db = mat['FIELD_PARAMS']['alpha'][0,0][0][0]
         frequency = mat['FIELD_PARAMS']['Frequency'][0,0][0][0]
         alpha_np = alpha_db * frequency / 8.616
+
+        # print(c, alpha_db, frequency, alpha_np)
 
         node_ids, intensity = self.interpolate_field_to_dyna_intensities(field_load_file)
         point_loads = self.calculate_point_loads_from_field_intensities(intensity, normalization_isppa, alpha_np, c)