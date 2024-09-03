82ffa92b579fc9b60335b2df8490e48631368192
Joey Richardson <richardson.joey.b@gmail.com>
2024-09-03 16:22:30 -0400

    Added functions for meta schedulers

--- fem/dyna/find_folders.py
+++ fem/dyna/find_folders.py
@@ -1,6 +1,28 @@
 import os
 import sys
 import glob
+import json
+import pathlib
+
+
+def load_json_file(filepath):
+    if not os.path.exists(filepath):
+        return []
+
+    try:
+        with open(filepath, "r") as file:
+            return json.load(file)
+    except (json.JSONDecodeError, OSError) as e:
+        # Handle JSON errors or file read errors
+        return []
+
+
+def write_to_json_file(data, filepath):
+    try:
+        with open(filepath, "w") as file:
+            json.dump(data, file, indent=4)
+    except OSError as e:
+        print(f"Error saving to file: {e}")
 
 
 def write_folders_to_file(folder_list, file_path):
@@ -9,31 +31,99 @@ def write_folders_to_file(folder_list, file_path):
             file.write(folder + "\n")
 
 
-def find_incomplete_dyna_sim_folders(base_path):
+def read_folders_from_file(file_path):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        # Strip newline characters from each line
+        file_list = [line.strip() for line in lines]
+    return file_list
+
+
+def find_all_sim_folders(base_path):
     folders = []
     for dirpath, dirnames, filenames in os.walk(base_path):
-        if "Master.dyn" in filenames and "res_sim.mat" not in filenames:
+        if "Master.dyn" in filenames:
             folders.append(dirpath)
     return folders
 
 
-def find_disp_dat_folders(base_path):
-    folders = []
-    for dirpath, dirnames, filenames in os.walk(base_path):
-        if "disp.dat" in filenames:
-            folders.append(dirpath)
-    return folders
+def find_incomplete_dyna_sim_folders(sim_folder_list):
+    incomplete_sim_folders = []
+    for folder in sim_folder_list:
+        filenames = [x.name for x in pathlib.Path(folder).glob("*") if x.is_file()]
+        if (
+            "Master.dyn" in filenames
+            and "CLEANUP_READY.txt" not in filenames
+            and ("res_sim.mat" not in filenames or "disp.dat" not in filenames)
+        ):
+            incomplete_sim_folders.append(folder)
+    return incomplete_sim_folders
 
 
-def find_incomplete_ultratrack_sim_folders(base_path):
-    folders = []
-    for dirpath, dirnames, filenames in os.walk(base_path):
-        if "ultratrack_params.mat" in filenames:
-            rf_lines_files = glob.glob(dirpath + "/" + "all_rf_lines*.mat")
-            res_tracksim_files = glob.glob(dirpath + "/" + "res_tracksim*.mat")
-            if len(rf_lines_files) and len(res_tracksim_files):
-                folders.append(dirpath)
-    return folders
+def find_cleanup_ready_sim_folders(sim_folder_list):
+    cleanup_ready_sim_folders = []
+    for folder in sim_folder_list:
+        filenames = [x.name for x in pathlib.Path(folder).glob("*") if x.is_file()]
+        if (
+            "Master.dyn" in filenames
+            and "CLEANUP_READY.txt" in filenames
+            and ("res_sim.mat" not in filenames or "disp.dat" not in filenames)
+        ):
+            cleanup_ready_sim_folders.append(folder)
+    return cleanup_ready_sim_folders
+
+
+def find_complete_dyna_sim_folders(sim_folder_list):
+    complete_sim_folders = []
+    for folder in sim_folder_list:
+        filenames = [x.name for x in pathlib.Path(folder).glob("*") if x.is_file()]
+        if (
+            "Master.dyn" in filenames
+            and "CLEANUP_READY.txt" not in filenames
+            and "res_sim.mat" in filenames
+            and "disp.dat" in filenames
+        ):
+            complete_sim_folders.append(folder)
+    return complete_sim_folders
+
+
+def find_ultratrack_folders(folder_list):
+    ultratrack_folders = []
+    for folder in folder_list:
+        for dirpath, dirnames, filenames in os.walk(folder):
+            if "ultratrack_params.mat" in filenames:
+                ultratrack_folders.append(dirpath)
+    return ultratrack_folders
+
+
+def find_complete_ultratrack_sim_folders(ultratrack_folder_list):
+    complete_ultratrack_folders = []
+    for folder in ultratrack_folder_list:
+        rf_lines_files = list(pathlib.Path(folder).glob("all_rf_lines*.mat"))
+        res_tracksim_files = list(pathlib.Path(folder).glob("res_tracksim*.mat"))
+
+        if len(rf_lines_files) and len(res_tracksim_files):
+            complete_ultratrack_folders.append(folder)
+    return complete_ultratrack_folders
+
+
+# def find_disp_dat_folders(base_path):
+#     folders = []
+#     for dirpath, dirnames, filenames in os.walk(base_path):
+#         if "disp.dat" in filenames:
+#             folders.append(dirpath)
+#     return folders
+
+
+# def find_incomplete_ultratrack_sim_folders(base_path):
+#     folders = []
+#     for dirpath, dirnames, filenames in os.walk(base_path):
+#         if "ultratrack_params.mat" in filenames:
+#             rf_lines_files = glob.glob(dirpath + "/" + "all_rf_lines*.mat")
+#             res_tracksim_files = glob.glob(dirpath + "/" + "res_tracksim*.mat")
+#             if len(rf_lines_files) and len(res_tracksim_files):
+#                 folders.append(dirpath)
+#     return folders
 
 
 if __name__ == "__main__":
---
+++ fem/dyna/slurm.py
@@ -0,0 +1,308 @@
+import subprocess
+from simple_slurm import Slurm
+import os
+import pathlib
+
+CPUS_PER_NODE = 44
+ONYEN = os.environ["USER"]
+DYNA_DEBUG_RESOURCES = dict(
+    partition="debug_queue",
+    nodes=1,
+    ntasks=CPUS_PER_NODE * 1,  # cpus per node * number of nodes
+    time="00:02:00",
+    job_name="dyna",
+    mail_type="FAIL",
+    mail_user=f"{ONYEN}@email.unc.edu",
+)
+DYNA_2112_RESOURCES = dict(
+    partition="2112_queue",
+    nodes=13,
+    ntasks=CPUS_PER_NODE * 13,  # cpus per node * number of nodes
+    time="3:00:00",
+    job_name="dyna",
+    mail_type="FAIL",
+    mail_user=f"{ONYEN}@email.unc.edu",
+)
+DYNA_528_RESOURCES = dict(
+    partition="528_queue",
+    nodes=4,
+    ntasks=CPUS_PER_NODE * 4,  # cpus per node * number of nodes
+    time="5:00:00",
+    job_name="dyna",
+    mail_type="FAIL",
+    mail_user=f"{ONYEN}@email.unc.edu",
+)
+# DYNA_528_RESOURCES = dict(
+#     partition="528_queue",
+#     nodes=12,
+#     ntasks=CPUS_PER_NODE * 12,  # cpus per node * number of nodes
+#     time="3:30:00",
+#     job_name="dyna",
+#     mail_type="FAIL",
+#     mail_user=f"{ONYEN}@email.unc.edu",
+# )
+CLEANUP_RESOURCES = dict(
+    partition="cleanup_queue",
+    nodes=1,
+    ntasks=1,
+    time="6:00:00",
+    job_name="cleanup",
+    mem="75G",
+    mail_type="FAIL",
+    mail_user=f"{ONYEN}@email.unc.edu",
+)
+ULTRATRACK_RESOURCES = dict(
+    partition="general",
+    nodes=1,
+    ntasks=1,
+    time="16:00:00",
+    job_name="ultratrack",
+    mem="4G",
+    cpus_per_task=8,
+    mail_type="FAIL",
+    mail_user=f"{ONYEN}@email.unc.edu",
+)
+
+
+CLEANUP_SIMULATION_FILES_LINES = r"""
+# Cleanup files from any previous attempts to run simulation
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo -e "\nClean up temporary simulation files"
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+cleanup_files_start_time=$(date +%s)
+
+find . -name "adptmp" -type f -delete
+find . -name "bg_switch*" -type f -delete
+find . -name "binout*" -type f -delete
+find . -name "core*" -type f -delete
+find . -name "disk*" -type f -delete
+find . -name "d3dump*" -type f -delete
+find . -name "d3*" -type f -delete
+find . -name "glstat" -type f -delete
+find . -name "kill_by_pid" -type f -delete
+find . -name "load_profile*" -type f -delete
+find . -name "*lsda*" -type f -delete
+find . -name "mes0*" -type f -delete
+find . -name "mpptbin*" -type f -delete
+find . -name "part_des*" -type f -delete
+find . -name "rf0*" -type f -delete
+find . -name "scr*" -type f -delete
+find . -name "spool*" -type f -delete
+find . -name "nodout" -type f -delete
+find . -name "status.out" -type f -delete
+find . -name "CLEANUP_READY.txt" -type f -delete
+echo -e "Runtime [cleanup_files]: $(($(date +%s) - $cleanup_files_start_time))"
+"""
+
+PWD_LINES = r"""
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo -e "\nCurrent working directory: $PWD"
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n"
+"""
+
+DYNA_SIMULATION_LINES = r"""
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo -e "\nSimulating ARF emissions applied to FEM mesh"
+echo -e "\n--partition $SLURM_JOB_PARTITION"
+echo -e "\n--job $SLURM_JOB_ID"
+echo -e "\n--job_array $SLURM_ARRAY_JOB_ID"
+echo -e "\n--job_array_task_id $SLURM_ARRAY_TASK_ID"
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\n"
+$MPI_HOME/bin/mpirun -np $SLURM_NPROCS lsdyna_mpp_d i="Master.dyn" memory=20000m memory2=500m
+"""
+
+L2A_LINES = r"""
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo -e "\nGenerate binary nodout file from binout* FEM files"
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+l2a_start_time=$(date +%s)
+
+l2a_s binout*   # could potentially use l2a_d for double precision if necessary in the future
+
+echo -e "Runtime [l2a_s]: $(($(date +%s) - $l2a_start_time))"
+"""
+
+CREATE_DISP_DAT_LINES = r"""
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo -e "\nCreating disp.dat file"
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+create_disp_dat_start_time=$(date +%s)
+
+# C function path
+source_file="$fem_folder/fem/post/create_disp_dat_fast.c"
+
+# Executatble path
+executable="$fem_folder/fem/post/create_disp_dat_fast"
+
+# Check if the executable exists
+if [ -e "$executable" ]; then
+    echo "Executable already exists."
+else
+    # Check if the source file exists
+    if [ -e "$source_file" ]; then
+        # Compile the source file to create the executable
+        gcc -o "$executable" "$source_file"
+        echo "Compilation successful."
+    else
+        echo "Error: Source file '$source_file' not found."
+    fi
+fi
+
+# Run compiled create_disp_dat_fast
+$executable -o ./disp.dat -i ./nodout
+
+echo -e "Runtime [create_disp_dat]: $(($(date +%s) - $create_disp_dat_start_time))"
+"""
+
+CREATE_RES_SIM_LINES = r"""
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+echo -e "\nConvert disp.dat to res_sim.mat"
+echo -e "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n"
+create_res_sim_start_time=$(date +%s)
+
+# Run function to convert from disp.dat to res_sim.mat
+python $fem_folder/fem/dyna/post.py
+
+echo -e "Runtime [create_res_sim]: $(($(date +%s) - $create_res_sim_start_time))"
+"""
+
+
+def get_job_ids(user, partition, job_state="PD,R"):
+    try:
+        # Command to get the user's queued jobs in the specified partition
+        cmd = [
+            "squeue",
+            "-u",
+            user,
+            "-p",
+            partition,
+            "-r",  # different line for each job in array
+            "-h",
+            "-t",
+            job_state,
+            "-o",
+            "%i",
+        ]
+
+        # Run the command and capture the output
+        result = subprocess.run(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+
+        # Check if there's an error
+        if result.returncode != 0:
+            print(f"Error: {result.stderr}")
+            return None
+
+        # Process the output to get an array of job ids
+        job_ids = result.stdout.strip().splitlines()
+        return job_ids
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def setup_dyna_simulation_folders(log_folder, n_sim_folders, sim_setup_script_name):
+    log_folder.mkdir(parents=True, exist_ok=True)
+
+    ONYEN = os.environ["USER"]
+    slurm = Slurm(
+        output=f"{log_folder}/%A-%a-out.log",
+        error=f"{log_folder}/%A-%a-err.log",
+        partition="general",
+        nodes=1,
+        ntasks=1,
+        time="01:00:00",
+        job_name="sim_setup",
+        mail_type="FAIL",
+        mail_user=f"{ONYEN}@email.unc.edu",
+        array=range(0, n_sim_folders),
+    )
+    slurm.add_cmd("source activate fem")
+    slurm.add_cmd(f"python {sim_setup_script_name}")
+    return slurm
+
+
+def setup_ultratrack_simulation_folders(
+    log_folder, ultratrack_setup_script_full_path, sim_folder_filename
+):
+    log_folder.mkdir(parents=True, exist_ok=True)
+    ultratrack_setup_script_full_path = pathlib.Path(ultratrack_setup_script_full_path)
+    config_folder = ultratrack_setup_script_full_path.parents[0]
+    ultratrack_setup_script_name = ultratrack_setup_script_full_path.stem
+
+    ONYEN = os.environ["USER"]
+    slurm = Slurm(
+        output=f"{log_folder}/%A-%a-out.log",
+        error=f"{log_folder}/%A-%a-err.log",
+        partition="general",
+        nodes=1,
+        ntasks=1,
+        time="01:00:00",
+        job_name="ultratrack_setup",
+        mail_type="FAIL",
+        mail_user=f"{ONYEN}@email.unc.edu",
+    )
+    slurm.add_cmd(f"config_folder={config_folder}")
+    slurm.add_cmd(f"sim_folder_filename={sim_folder_filename}")
+    slurm.add_cmd(
+        f"""matlab -batch \"addpath('${{config_folder}}'); {ultratrack_setup_script_name}('${{sim_folder_filename}}')\""""
+    )
+    return slurm
+
+
+def setup_dyna_job(log_folder, sim_folder, resources):
+    log_folder.mkdir(parents=True, exist_ok=True)
+
+    slurm = Slurm(
+        **resources,
+        output=f"{log_folder}/%A-%a-out.log",
+        error=f"{log_folder}/%A-%a-err.log",
+    )
+    slurm.add_cmd("start_time=$(date +%s)")
+    slurm.add_cmd(f'folder="{sim_folder}"')
+    slurm.add_cmd('cd "$folder"')
+    slurm.add_cmd(PWD_LINES)
+    slurm.add_cmd(CLEANUP_SIMULATION_FILES_LINES)
+    slurm.add_cmd(DYNA_SIMULATION_LINES)
+    slurm.add_cmd('echo -e "Runtime [lsdyna_mpp_d]: $(($(date +%s) - $start_time))"')
+    slurm.add_cmd("touch CLEANUP_READY.txt")
+    return slurm
+
+
+def setup_cleanup_job(log_folder, sim_folder, resources):
+    log_folder.mkdir(parents=True, exist_ok=True)
+
+    slurm = Slurm(
+        **resources,
+        output=f"{log_folder}/%A-%a-out.log",
+        error=f"{log_folder}/%A-%a-err.log",
+    )
+    slurm.add_cmd("start_time=$(date +%s)")
+    slurm.add_cmd(f'folder="{sim_folder}"')
+    slurm.add_cmd('cd "$folder"')
+    slurm.add_cmd('fem_folder="$HOME/repos/fem"')
+    slurm.add_cmd(PWD_LINES)
+    slurm.add_cmd(L2A_LINES)
+    slurm.add_cmd(CREATE_DISP_DAT_LINES)
+    slurm.add_cmd(CREATE_RES_SIM_LINES)
+    slurm.add_cmd(CLEANUP_SIMULATION_FILES_LINES)
+    slurm.add_cmd('echo -e "Runtime [total]: $(($(date +%s) - $start_time))"')
+    slurm.add_cmd("sleep 5m")  # make sure cleanup is done before exiting job
+    return slurm
+
+
+def setup_ultratrack_job(log_folder, sim_folder, ultratrack_script_name, resources):
+    log_folder.mkdir(parents=True, exist_ok=True)
+
+    slurm = Slurm(
+        **resources,
+        output=f"{log_folder}/%A-%a-out.log",
+        error=f"{log_folder}/%A-%a-err.log",
+    )
+    slurm.add_cmd(f'sim_folder="{sim_folder}"')
+    slurm.add_cmd(
+        f"""matlab -batch \"addpath(genpath(fullfile(getenv('HOME'), 'repos', 'ultratrack'))); {ultratrack_script_name}('${{sim_folder}}')\""""
+    )
+    return slurm
